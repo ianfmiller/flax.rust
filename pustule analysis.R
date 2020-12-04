72fc836c02fc8f30c14602ed1abef33cfe110830
ifmiller <ifmiller@princeton.edu>
2020-12-04 14:43:37 -0500

    data processing

--- pustule analysis.R
+++ pustule analysis.R
@@ -1,40 +1,24 @@
+library(mgcv)
+
+# clean data
+
+## load data
 pustules<-read.csv("~/Documents/GitHub/flax.rust/data/pustule measurements.csv")
+
+## add in area
 area<-pi*(pustules$max.diam..mm./4)*(pustules$min.diam..mm./4)
 pustules$date<-as.Date(pustules$date,tryFormats = "%m/%d/%y")
 pustules$area<-area
 
+## clean data
 pustules<-pustules[which(pustules$pustule.ID.confidence=="Yes"),]
 pustules<-pustules[which(pustules$area>0),]
-hist(pustules$area)
 
-### plot trajectories
-plot(c(min(pustules$date),max(pustules$date)),c(0,max(pustules$area)),type="n",xlab="date",ylab="pustule area")
+## make new data object for change in pustule size
 
-for (tag in unique(pustules$tag))
-{
-  sub.pustules1<-pustules[which(pustules$tag==tag),]
-  
-  for (color in unique(sub.pustules1$color))
-  {
-    sub.pustules2<-sub.pustules1[which(sub.pustules1$color==color),]
-    
-    for(leaf.iteration in unique(sub.pustules2$leaf.iteration)) 
-    {
-      sub.pustules3<-sub.pustules2[which(sub.pustules2$leaf.iteration==leaf.iteration),]
-      
-      for(pustule.number in unique(sub.pustules3$pustule.number))
-      {
-        sub.pustules4<-sub.pustules3[which(sub.pustules3$pustule.number==pustule.number),]
-        sub.pustules4<-sub.pustules4[order(sub.pustules4$date),]
-        points(sub.pustules4$date,sub.pustules4$area,col="grey60",type="l",lwd=.5)
-      }
-    }
-  }
-}
-
-### plot change
 start.vals<-c()
-change<-c()
+end.vals<-c()
+days<-c()
 
 for (tag in unique(pustules$tag))
 {
@@ -52,16 +36,66 @@ for (tag in unique(pustules$tag))
       {
         sub.pustules4<-sub.pustules3[which(sub.pustules3$pustule.number==pustule.number),]
         sub.pustules4<-sub.pustules4[order(sub.pustules4$date),]
-        for(i in 1:(dim(sub.pustules4)[1]-1))
+        if(dim(sub.pustules4)[1]>=2)
         {
-          start.vals<-c(start.vals,sub.pustules4[i,"area"])
-          change<-c(change,sub.pustules4[i+1,"area"]-sub.pustules4[i,"area"])
+          for(i in 1:(dim(sub.pustules4)[1]-1))
+          {
+            date0<-sub.pustules4[i,"date"]
+            date1<-sub.pustules4[i+1,"date"]
+            site<-sub.pustules4[i,"site"]
+            
+            start.vals<-c(start.vals,sub.pustules4[i,"area"])
+            end.vals<-c(end.vals,sub.pustules4[i+1,"area"])
+            days<-c(days,date1-date0)
+            
+          
+            
+          } 
         }
       }
     }
   }
 }
 
-plot(start.vals,change,col="grey")
+delta.pustules<-data.frame(area=start.vals,area.next=end.vals,time=days)
+
+# visualize data
+
+## size histogram
+hist(pustules$area,main="pustule area",breaks=100)
+
+## plot trajectories
+plot(c(min(pustules$date),max(pustules$date)),c(0,max(pustules$area)),type="n",xlab="date",ylab="pustule area")
+i<-0
+
+plot.cols<-sample(rainbow(2042))
+
+for (tag in unique(pustules$tag))
+{
+  sub.pustules1<-pustules[which(pustules$tag==tag),]
+  
+  for (color in unique(sub.pustules1$color))
+  {
+    sub.pustules2<-sub.pustules1[which(sub.pustules1$color==color),]
+    
+    for(leaf.iteration in unique(sub.pustules2$leaf.iteration)) 
+    {
+      sub.pustules3<-sub.pustules2[which(sub.pustules2$leaf.iteration==leaf.iteration),]
+      
+      for(pustule.number in unique(sub.pustules3$pustule.number))
+      {
+        i<-i+1
+        sub.pustules4<-sub.pustules3[which(sub.pustules3$pustule.number==pustule.number),]
+        sub.pustules4<-sub.pustules4[order(sub.pustules4$date),]
+        points(sub.pustules4$date,sub.pustules4$area,col=plot.cols[i],type="l",lwd=.5)
+      }
+    }
+  }
+}
+
+## plot change
+plot(delta.pustules$area,delta.pustules$area.next,col="grey")
 abline(0,1)
 
+# analyze data
+mod<-gam(area.next~s(area)+s(days),data = delta.pustules)
