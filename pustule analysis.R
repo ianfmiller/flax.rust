187611ba36fef59b29836a3fff068d7c31700943
ifmiller <ifmiller@princeton.edu>
2020-12-07 16:17:40 -0500

    adding in weath vars

--- pustule analysis.R
+++ pustule analysis.R
@@ -19,6 +19,10 @@ pustules<-pustules[which(pustules$area>0),]
 ## make new data object for change in pustule size
 temp.rh.sub.func<-function(x,lower.bound,upper.bound) {out<-subset(x,temp.c>=lower.bound); out<-subset(out,temp.c<=upper.bound); out}
 
+tags<-c()
+stem.iters<-c()
+leaf.iters<-c()
+pustule.nums<-c()
 start.vals<-c()
 end.vals<-c()
 days<-c()
@@ -31,6 +35,12 @@ mean.dew.point<-c()
 temp.dew.point.days<-c()
 temp.16.22.dew.point.days<-c()
 temp.7.30.dew.point.days<-c()
+mean.wetnesss<-c()
+tot.rains<-c()
+mean.solars<-c()
+mean.wind.speeds<-c()
+mean.gust.speeds<-c()
+
 
 
 for (tag in unique(pustules$tag)) #916, 917, 920
@@ -73,6 +83,11 @@ for (tag in unique(pustules$tag)) #916, 917, 920
             temp.rh.sub<-subset(temp.rh.sub,!is.na(temp.c)) #throw out NAs
             temp.rh.sub<-cbind(temp.rh.sub,interval.length=c(diff(as.numeric(temp.rh.sub$date.time))/(60*60*24),NA)) #add interval length in days
             
+            #subset weather data to relevant window
+            weath.sub<-subset(all.weath,site==site)
+            weath.sub<-subset(weath.sub,date<=date1) #pull out relevant data
+            weath.sub<-subset(weath.sub,date>=date0) #pull out relevant data
+            
             #calculate environmental variable metrics
             new.temp.days.16.22<-sum(temp.rh.sub.func(temp.rh.sub,16,22)$temp.c*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T) #temperature days for temp between 16 and 22 celsius
             new.temp.days.7.30<-sum(temp.rh.sub.func(temp.rh.sub,7,30)$temp.c*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T) #temperature days for temp between 7 and 30 celsius
@@ -81,10 +96,17 @@ for (tag in unique(pustules$tag)) #916, 917, 920
             new.dew.point.days<-mean(temp.rh.sub$dew.pt.c,na.rm = T) #Dew point days
             new.mean.dew.point<-mean(temp.rh.sub$dew.pt.c,na.rm = T) #temperature days
 
-            ##calculate joint environmental variable metrics--accounts for temporal co-occurence of environmental variables
+            #calculate joint environmental variable metrics--accounts for temporal co-occurence of environmental variables
             new.temp.dew.point.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T)
             new.temp.16.22.dew.point.days<-sum(temp.rh.sub.func(temp.rh.sub,16,22)$temp.c*temp.rh.sub.func(temp.rh.sub,16,22)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T)
             new.temp.7.30.dew.point.days<-sum(temp.rh.sub.func(temp.rh.sub,7,30)$temp.c*temp.rh.sub.func(temp.rh.sub,7,30)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T)
+            
+            #calculate weather metrics
+            new.mean.wetness<-mean(weath.sub$wetness)
+            new.tot.rain<-sum(weath.sub$rain)
+            new.mean.solar<-mean(weath.sub$solar.radiation)
+            new.mean.wind.speed<-mean(weath.sub$wind.speed)
+            new.mean.gust.speed<-mean(weath.sub$gust.speed)
             
             #pull out core predictors
             start.val<-sub.pustules4[i,"area"]
@@ -92,6 +114,11 @@ for (tag in unique(pustules$tag)) #916, 917, 920
             delta.days<-date1-date0
             
             #store values
+            tags<-c(tags,tag)
+            stem.iters<-c(stem.iters,color)
+            leaf.iters<-c(leaf.iters,leaf.iteration)
+            pustule.nums<-c(pustule.nums,pustule.number)
+            
             start.vals<-c(start.vals,start.val)
             end.vals<-c(end.vals,end.val)
             days<-c(days,delta.days)
@@ -106,6 +133,13 @@ for (tag in unique(pustules$tag)) #916, 917, 920
             temp.16.22.dew.point.days<-c(temp.16.22.dew.point.days,new.temp.16.22.dew.point.days)
             temp.7.30.dew.point.days<-c(temp.7.30.dew.point.days,new.temp.7.30.dew.point.days)
             
+            mean.wetnesss<-c(mean.wetnesss,new.mean.wetness)
+            tot.rains<-c(tot.rains,new.tot.rain)
+            mean.solars<-c(mean.solars,new.mean.solar)
+            mean.wind.speeds<-c(mean.wind.speeds,new.mean.wind.speed)
+            mean.gust.speeds<-c(mean.gust.speeds,new.mean.gust.speed)
+            
+            
           } 
         }
       }
@@ -113,7 +147,9 @@ for (tag in unique(pustules$tag)) #916, 917, 920
   }
 }
 
-delta.pustules<-data.frame(area=start.vals,area.next=end.vals,time=days,temp.days.16.22=temp.days.16.22,temp.days.7.30=temp.days.7.30,temp.days=temp.days,mean.temp=mean.temp,dew.point.days=dew.point.days,mean.dew.point=mean.dew.point,temp.16.22.dew.point.days=temp.16.22.dew.point.days,temp.7.30.dew.point.days=temp.7.30.dew.point.days,temp.dew.point.days=temp.dew.point.days)
+delta.pustules<-data.frame(tag=factor(tags),stem.iter=stem.iters,leaf.iter=leaf.iters,pustule.num=pustule.nums,area=start.vals,area.next=end.vals,time=days,
+                           temp.days.16.22=temp.days.16.22,temp.days.7.30=temp.days.7.30,temp.days=temp.days,mean.temp=mean.temp,dew.point.days=dew.point.days,mean.dew.point=mean.dew.point,temp.16.22.dew.point.days=temp.16.22.dew.point.days,temp.7.30.dew.point.days=temp.7.30.dew.point.days,temp.dew.point.days=temp.dew.point.days,
+                           mean.wetness=mean.wetnesss,tot.rain=tot.rains,mean.solar=mean.solars,mean.wind.speed=mean.wind.speeds,mean.gust.speed=mean.gust.speeds)
 
 # visualize data
 
@@ -183,4 +219,4 @@ plot(delta.pustules$temp.dew.point.days,delta.pustules$area.next)
 plot(delta.pustules$temp.dew.point.days,delta.pustules$area-delta.pustules$area.next)
 
 # analyze data
-mod<-gam(area.next~s(area)+s(days),data = delta.pustules)
+mod<-gam(area.next~s(area)+s(time)+s(temp.dew.point.days),data = delta.pustules)
