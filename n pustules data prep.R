f867a8146a9be2ec5e35d2035239c3bd515ad5ff
ifmiller <ifmiller@princeton.edu>
2021-01-06 17:30:46 -0500

    add pustule number data prep file

---
+++ n pustules data prep.R
@@ -0,0 +1,3 @@
+# load model and data from pustule area analysis
+source("~/Documents/GitHub/flax.rust/pustule analysis data prep.R")
+pustule.model<-readRDS("~/Documents/GitHub/flax.rust/data/models/pustule.model.RDS")
