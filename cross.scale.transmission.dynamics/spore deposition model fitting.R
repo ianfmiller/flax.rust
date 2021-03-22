a9cfbaf5fd97fa57eb8393ed6d4864b93337b398
ifmiller <ifmiller@princeton.edu>
2021-03-22 16:45:50 -0400

    fix parentheses, re-structure deviance vis

--- cross.scale.transmission.dynamics/spore deposition model fitting.R
+++ cross.scale.transmission.dynamics/spore deposition model fitting.R
@@ -21,7 +21,7 @@ opt<-optim(par=c(1.470363e-02,1.355715e-02,1.128306e-06),fn=param.search.optim.d
 ### total sum of squares = 15563.23 (total sum of squares = sum((spores/squares - mean(spores/squares))^2) )
 ### x<-c(4.772329e-01,2.261641e-02,6.285425e-06) #OPT1 output value = 14637.1 for one day
 ### x<-c(5.543098e+00,1.907743e-02,3.231231e-06) #OPT1 output value = 14651.75 for two days ALMOST NO DECAY IN X DIRECTION but similar output value for all values of cval (opt$par[1])
-### x<-c1.177116e-02,1.185136e-02,1.263117e-06) #OPT1 output value = 14843.28 for full period
+### x<-c(1.177116e-02,1.185136e-02,1.263117e-06) #OPT1 output value = 14843.28 for full period
 
 ## visualize kernel
 test.mat<-data.frame(x=rep(seq(-1,1,.01),each=201),y=rep(seq(-1,1,.01),times=201))
@@ -30,9 +30,9 @@ res.mat<-matrix(out,201,201,byrow = T)
 filled.contour(x=seq(-1,1,.01),y=seq(-1,1,.01),z=res.mat)
 
 ## visualize fit
-pred.mat<-param.search.optim.decay.plume(opt$par,return.vec=T)
+pred.mat<-param.search.optim.decay.plume(opt$par,return.out=T)
 plot(pred.mat$obs,pred.mat$pred)
-plot(pred.mat$obs,pred.mat$obs-pred.mat$pred)
+plot(pred.mat$obs,pred.mat$pred-pred.mat$obs)
 
 
 # visualize parameter search
