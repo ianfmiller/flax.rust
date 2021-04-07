88354bd251662ec6c123004b0bf283d196180f5e
ifmiller <ifmiller@princeton.edu>
2021-04-07 18:09:39 -0400

    reformat

--- cross.scale.transmission.dynamics/simulate plant inf intens prediction from model.R
+++ cross.scale.transmission.dynamics/simulate plant inf intens prediction from model.R
@@ -1,3 +1,2 @@
 # to be insertd into code similar to that in 'predict plant inf intens change funcs.R'
-bootMer(plant.model, FUN=function(x)predict(x, pred.data, re.form=NA),
-        nsim=1)$t
+bootMer(plant.model, FUN=function(x)predict(x, pred.data, re.form=NA),nsim=1)$t
