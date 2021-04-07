466e87c8ada9df3ce2e829f861688a1a43124cb2
ifmiller <ifmiller@princeton.edu>
2021-04-07 18:10:26 -0400

    add enviro vars

--- building foi dataset.R
+++ building foi dataset.R
@@ -4,6 +4,9 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
   source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant loc dataset building.R")
   source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/predict plant inf intens change funcs.R")
   source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/spore deposition functions tilt.R")
+  pustule.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/pustule.model.RDS")
+  n.pustules.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/n.pustules.model.RDS")
+  plant.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/plants.model.RDS")
   plant.inf.intens<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/summarized data/plants.RDS")
   # Time periods for fitting glm of infection~foi to data:
   #CC: 6/22(first data)->7/27(last prediction)
@@ -94,7 +97,10 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
   data.dates<-list("CC"=c("2020-06-22","2020-06-29","2020-07-06","2020-07-13","2020-07-20"),"BT"=c("2020-06-24","2020-07-01"),"GM"=c("2020-06-23","2020-06-30","2020-07-02","2020-07-07","2020-07-09"),"HM"=c("2020-06-25","2020-07-02","2020-07-07","2020-07-09"))
   
   # build empty data object for foi vs outcome data
-  foi.data<-data.frame("site"=character(),"date"=as.Date(character()),"status"=character(),"status.next"=character(),"tag"=character(),"X"=numeric(),"Y"=numeric(),"x"=numeric(),"y"=numeric(),"foi"=numeric())
+  foi.data<-data.frame("site"=character(),"date"=as.Date(character()),"status"=character(),"status.next"=character(),"tag"=character(),"X"=numeric(),"Y"=numeric(),"x"=numeric(),"y"=numeric(),"first.obs.height"=numeric(),"foi"=numeric(),
+                       "temp.days"=numeric(),"temp.days.16.22"=numeric(),"temp.days.7.20"=numeric(),"dew.point.days"=numeric(),"wetness.days"=numeric(),"tot.rain"=numeric(),"solar.days"=numeric(),"wind.speed.days"=numeric(),
+                       "gust.speed.days"=numeric(),"temp.16.22.dew.point.days"=numeric(),"temp.7.30.dew.point.days"=numeric(),"temp.wetness.days"=numeric(),"temp.16.22.wetness.days"=numeric(),"temp.7.30.wetness.days"=numeric(),
+                       "pred.pustule.diam.growth"=numeric(),"pred.pustule.num.increase"=numeric(),"pred.plant.inf.intens.increase"=numeric())
   
   # fill data object
   for(site in sites)
@@ -116,6 +122,64 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
     {
       date0<-epi.obs.dates[which(sites==site)][[1]][date.index] ### date of current observation
       date1<-epi.obs.dates[which(sites==site)][[1]][date.index+1] ### date of next observation
+      delta.days<-as.numeric(as.Date(date1)-as.Date(date0))
+      ## get weather/enviro predictors
+      
+      ### subset temp data to relevant window
+      temp.rh.sub<-all.temp.rh[which(all.temp.rh$site==site),] #pull out temp data for site
+      temp.rh.sub<-subset(temp.rh.sub,date.time<=date1) #### pull out relevant data
+      temp.rh.sub<-subset(temp.rh.sub,date.time>=date0) #### pull out relevant data
+      temp.rh.sub<-subset(temp.rh.sub,!is.na(temp.c)) #### throw out NAs
+      temp.rh.sub<-cbind(temp.rh.sub,interval.length=c(diff(as.numeric(temp.rh.sub$date.time))/(60*60*24),NA)) #add interval length in days
+      
+      #### subset weather data to relevant window
+      weath.sub<-all.weath[which(all.weath$site==site),] #pull out weath data for site
+      weath.sub<-subset(weath.sub,date<=date1) #### pull out relevant data
+      weath.sub<-subset(weath.sub,date>=date0) #### pull out relevant data
+      weath.sub<-cbind(weath.sub,interval.length=c(diff(as.numeric(weath.sub$date))/(60*60*24),NA))
+      
+      #### calculate environmental variable metrics
+      new.temp.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$interval.length,na.rm = T) #temperature days
+      new.temp.days.16.22<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T) #time (in days) during which temp between 16 and 22 celsius
+      new.temp.days.7.30<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T) #time (in days) during which temp between 7 and 30 celsius
+      new.dew.point.days<-sum(temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T) #Dew point days
+      
+      #calculate weather metrics
+      new.wetness.days<-sum(weath.sub$wetness*weath.sub$interval.length,na.rm = T)
+      new.tot.rain<-sum(weath.sub$rain,na.rm=T)
+      new.solar.days<-sum(weath.sub$solar.radiation*weath.sub$interval.length,na.rm = T)
+      new.wind.speed.days<-sum(weath.sub$wind.speed*weath.sub$interval.length,na.rm = T)
+      new.gust.speed.days<-sum(weath.sub$wind.direction*weath.sub$interval.length,na.rm = T)
+      
+      #calculate joint environmental variable metrics--accounts for temporal co-occurence of environmental variables
+      new.temp.dew.point.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T)
+      new.temp.16.22.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T)
+      new.temp.7.30.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T)
+      new.temp.wetness.days<-sum(weath.sub$temp*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
+      new.temp.16.22.wetness.days<-sum(weath.sub$temp.16.22*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
+      new.temp.7.30.wetness.days<-sum(weath.sub$temp.7.30*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
+      
+      #predict pustule growth from pustule growth model and enviro conditions
+      #pustule.model.vars<-names(fixef(pustule.model))[2:length(names(fixef(pustule.model)))]
+      pustule.model.new.area<-.01 #predict change for small pustule, arbitrarily pick .01
+      pustule.model.new.time<-1
+      obs.time<-delta.days
+      pustule.model.pred.data<-data.frame("area"=pustule.model.new.area,"time"=pustule.model.new.time,"temp.days.16.22"=new.temp.days.16.22/delta.days,"temp.16.22.dew.point.days"=new.temp.16.22.dew.point.days/delta.days,"temp.16.22.wetness.days"=new.temp.16.22.wetness.days/delta.days,"tot.rain"=new.tot.rain/delta.days)
+      pred.pustule.diam.growth<-predict(pustule.model,newdata=pustule.model.pred.data,re.form=~0)
+      
+      #predict change in number of pustules from enviro conditions
+      #n.pustule.model.vars<-names(fixef(n.pustule.model))[2:length(names(fixef(n.pustule.model)))]
+      n.pustules.model.new.n.pustules<-0 #included only for offset, picked 0 for ease of interpretability
+      obs.time<-delta.days
+      n.pustules.model.pred.data<-data.frame("n.pustules"=n.pustules.model.new.n.pustules,"temp.days.16.22"=new.temp.days.16.22/delta.days,"temp.16.22.wetness.days"=new.temp.16.22.wetness.days/delta.days)
+      pred.pustule.num.increase<-predict(n.pustules.model,newdata=n.pustules.model.pred.data,re.form=~0)
+      
+      #predict change in plant.inf.intensity from enviro conditions
+      #plant.model.vars<-names(fixef(plant.model))[2:length(names(fixef(plant.model)))]
+      plant.model.new.plant.inf.intens<-1
+      obs.time<-delta.days
+      plant.model.pred.data<-data.frame("plant.inf.intens"=plant.model.new.plant.inf.intens,"dew.point.days"=new.dew.point.days,"temp.7.30.dew.point.days"=new.temp.7.30.dew.point.days,"temp.wetness.days"=new.temp.wetness.days,"tot.rain"=new.tot.rain,"pred.pustule.num.increase"=pred.pustule.num.increase)
+      pred.plant.inf.intens.increase<-predict(plant.model,newdata=plant.model.pred.data,re.form=NA)
       
       sub.2.epi.data<-sub.1.epi.data[which(sub.1.epi.data$Date.First.Observed.Diseased<=date0),] ### epi data up until date0
       sub.3.epi.data<-sub.1.epi.data[which(sub.1.epi.data$Date.First.Observed.Diseased<=date1),] ### epi date up until date1
@@ -147,7 +211,10 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
           status<-ifelse(ID.string %in% epi.ID.strings,1,0) #### count plant as currently infected if its ID string shows up in the set of plants that have been infected by date0
           new.status<-ifelse(ID.string %in% epi.ID.strings.next,1,0) #### count plant as becoming infected by next obs if its ID string shows up in the set of plants that have been infected by date1
           foi<-foi.func(site=site,date0=date0,date1=date1,epi.data=corrected.epi,xcord=sub.loc.data[index,"X"]+sub.loc.data[index,"x"],ycord=sub.loc.data[index,"Y"]+sub.loc.data[index,"y"]) #### calculate foi experienced
-          foi.data<-rbind(foi.data,data.frame("site"=site,"date"=date0,"status"=status,"status.next"=new.status,"tag"=sub.loc.data[index,"tag"],"X"=sub.loc.data[index,"X"],"Y"=sub.loc.data[index,"Y"],"x"=sub.loc.data[index,"x"],"y"=sub.loc.data[index,"y"],"foi"=foi)) #### add new entry to data object
+          foi.data<-rbind(foi.data,data.frame("site"=site,"date"=date0,"status"=status,"status.next"=new.status,"tag"=sub.loc.data[index,"tag"],"X"=sub.loc.data[index,"X"],"Y"=sub.loc.data[index,"Y"],"x"=sub.loc.data[index,"x"],"y"=sub.loc.data[index,"y"],"first.obs.height"=sub.loc.data[index,"height.cm"],"foi"=foi,
+                                              "temp.days"=new.temp.days,"temp.days.16.22"=new.temp.days.16.22,"temp.days.7.30"=new.temp.days.7.30,"dew.point.days"=new.dew.point.days,"wetness.days"=new.wetness.days,"tot.rain"=new.tot.rain,"solar.days"=new.solar.days,"wind.speed.days"=new.wind.speed.days,
+                                              "gust.speed.days"=new.gust.speed.days,"temp.16.22.dew.point.days"=new.temp.16.22.dew.point.days,"temp.7.30.dew.point.days"=new.temp.7.30.dew.point.days,"temp.wetness.days"=new.temp.wetness.days,"temp.16.22.wetness.days"=new.temp.16.22.wetness.days,"temp.7.30.wetness.days"=new.temp.7.30.wetness.days,
+                                              "pred.pustule.diam.growth"=pred.pustule.diam.growth,"pred.pustule.num.increase"=pred.pustule.num.increase,"pred.plant.inf.intens.increase"=pred.plant.inf.intens.increase)) #### add new entry to data object
         }
         ### if that ID string is not unique
         if(length(which(ID.strings==ID.string))>1)
@@ -157,7 +224,10 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
           status<-ifelse(length(which(epi.ID.strings==ID.string))>=order,1,0) #### for plant that is the nth repeat, count as currently infected if at least n matching ID strings show up in epi.ID.strings
           new.status<-ifelse(length(which(epi.ID.strings.next==ID.string))>=order,1,0) #### for plant that is the nth repeat, count as becoming infected by next obs if at least n matching ID strings show up in epi.ID.strings.next
           foi<-foi.func(site=site,date0=date0,date1=date1,epi.data=corrected.epi,xcord=sub.loc.data[index,"X"]+sub.loc.data[index,"x"],ycord=sub.loc.data[index,"Y"]+sub.loc.data[index,"y"]) #### calculate foi experienced
-          foi.data<-rbind(foi.data,data.frame("site"=site,"date"=date0,"status"=status,"status.next"=new.status,"tag"=sub.loc.data[index,"tag"],"X"=sub.loc.data[index,"X"],"Y"=sub.loc.data[index,"Y"],"x"=sub.loc.data[index,"x"],"y"=sub.loc.data[index,"y"],"foi"=foi)) #### add new entry to data object
+          foi.data<-rbind(foi.data,data.frame("site"=site,"date"=date0,"status"=status,"status.next"=new.status,"tag"=sub.loc.data[index,"tag"],"X"=sub.loc.data[index,"X"],"Y"=sub.loc.data[index,"Y"],"x"=sub.loc.data[index,"x"],"y"=sub.loc.data[index,"y"],"first.obs.height"=sub.loc.data[index,"height.cm"],"foi"=foi,
+                                              "temp.days"=new.temp.days,"temp.days.16.22"=new.temp.days.16.22,"temp.days.7.30"=new.temp.days.7.30,"dew.point.days"=new.dew.point.days,"wetness.days"=new.wetness.days,"tot.rain"=new.tot.rain,"solar.days"=new.solar.days,"wind.speed.days"=new.wind.speed.days,
+                                              "gust.speed.days"=new.gust.speed.days,"temp.16.22.dew.point.days"=new.temp.16.22.dew.point.days,"temp.7.30.dew.point.days"=new.temp.7.30.dew.point.days,"temp.wetness.days"=new.temp.wetness.days,"temp.16.22.wetness.days"=new.temp.16.22.wetness.days,"temp.7.30.wetness.days"=new.temp.7.30.wetness.days,
+                                              "pred.pustule.diam.growth"=pred.pustule.diam.growth,"pred.pustule.num.increase"=pred.pustule.num.increase,"pred.plant.inf.intens.increase"=pred.plant.inf.intens.increase)) #### add new entry to data object
         }
         print(paste0("site = ",site," date = ",date0," ",index,"/",dim(sub.loc.data)[1]," complete"))
       }
