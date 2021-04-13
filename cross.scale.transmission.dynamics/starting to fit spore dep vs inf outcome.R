45d4962e7e564c9f646e960254acf78cc52b36c2
ifmiller <ifmiller@princeton.edu>
2021-04-12 21:38:34 -0400

    beautifying vis

--- cross.scale.transmission.dynamics/starting to fit spore dep vs inf outcome.R
+++ cross.scale.transmission.dynamics/starting to fit spore dep vs inf outcome.R
@@ -14,13 +14,9 @@ for (site in unique(epi$Site))
   {
     #plot(plant.loc.data$X+plant.loc.data$x,plant.loc.data$Y+plant.loc.data$y,xlim=c(0,10),ylim=c(0,20),main=paste0(site,as.Date(date,origin = "1970-01-01")),xlab="X",ylab="Y")
     sub.data.2<-sub.data.1[which(sub.data.1$Date.First.Observed.Diseased==date),]
-    points(sub.data.2$X+sub.data.2$x,sub.data.2$Y+sub.data.2$y,col="red")
-    Sys.sleep(2)
+    points(sub.data.2$X+sub.data.2$x,sub.data.2$Y+sub.data.2$y,col="red",cex=2)
+    Sys.sleep(1)
     new.infs<-c(new.infs,new.infs[length(new.infs)]+dim(sub.data.2)[1])
+    print(as.Date(date,origin="1970-01-01"))
   }
 }
-
-epi<-read.csv("~/Documents/GitHub/flax.rust/data/Epidemiology.csv")
-epi<-epi[which(epi$Year==2020),]
-plant.locs<-read.csv("~/Documents/GitHub/flax.rust/data/plant locs.csv")
-plant.locs<-plant.locs[which(plant.locs$Year==2020),]
