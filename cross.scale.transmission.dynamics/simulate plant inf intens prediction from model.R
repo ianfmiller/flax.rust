0100dd9c0fe4fb57750b9a0ce418d1408e7d25c7
ifmiller <ifmiller@princeton.edu>
2021-04-16 18:04:55 -0400

    doesn't work w/ gam

--- cross.scale.transmission.dynamics/simulate plant inf intens prediction from model.R
+++ cross.scale.transmission.dynamics/simulate plant inf intens prediction from model.R
@@ -1,2 +1,2 @@
 # to be insertd into code similar to that in 'predict plant inf intens change funcs.R'
-bootMer(plant.model, FUN=function(x)predict(x, pred.data, re.form=NA),nsim=1)$t
+bootMer(n.pustules.model, FUN=function(x)predict(x, pred.data, exclude='s(site)'),nsim=1)$t
