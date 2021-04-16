84630a0253bef9f0d0770f7f77e53336dbaaa731
ifmiller <ifmiller@princeton.edu>
2021-04-16 16:53:48 -0400

    add in all cross scale preds

--- cross.scale.transmission.dynamics/within host climate prediction functions.R
+++ cross.scale.transmission.dynamics/within host climate prediction functions.R
@@ -1,4 +1,8 @@
 source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/prep.enviro.data.R")
+pustule.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/pustule.model.RDS")
+n.pustules.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/n.pustules.model.RDS")
+plant.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/plants.model.RDS")
+
 sites<-c("CC","BT","GM","HM")
 
 pull.temp.mean.quantile.data<-function()
@@ -42,6 +46,9 @@ get.pred.data.temp.mean.quantile.pustule.model<-function(day.set,dummy.data,temp
   weath.sub<-subset(weath.sub,date>=date0) #pull out relevant data
   weath.sub<-cbind(weath.sub,interval.length=c(diff(as.numeric(weath.sub$date))/(60*60*24),NA))
   
+  #pull out core predictors
+  delta.days<-as.numeric(date1-date0)
+  
   #adjust temp data
   temp.rh.sub$temp.c<-temp.rh.sub$temp.c+temp.addition
   
@@ -66,13 +73,33 @@ get.pred.data.temp.mean.quantile.pustule.model<-function(day.set,dummy.data,temp
   new.temp.16.22.wetness.days<-sum(weath.sub$temp.16.22*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
   new.temp.7.30.wetness.days<-sum(weath.sub$temp.7.30*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
   
-  #pull out core predictors
-  delta.days<-date1-date0
+  #predict pustule growth from pustule growth model and enviro conditions
+  #pustule.model.vars<-names(fixef(pustule.model))[2:length(names(fixef(pustule.model)))]
+  pustule.model.new.area<-.01 #predict change for small pustule, arbitrarily pick .01
+  obs.time<-delta.days
+  pustule.model.pred.data<-data.frame("area"=pustule.model.new.area,"temp.days.16.22"=new.temp.days.16.22/delta.days,"dew.point.days"=new.dew.point.days/delta.days,"temp.16.22.dew.point.days"=new.temp.16.22.dew.point.days/delta.days,"temp.wetness.days"=new.temp.wetness.days/delta.days,"tot.rain"=new.tot.rain/delta.days)
+  pred.pustule.diam.growth<-predict(pustule.model,newdata=pustule.model.pred.data,re.form=~0)
+  
+  #predict change in number of pustules from enviro conditions
+  #n.pustule.model.vars<-names(fixef(n.pustule.model))[2:length(names(fixef(n.pustule.model)))]
+  n.pustules.model.new.n.pustules<-0 #included only for offset, picked 0 for ease of interpretability
+  obs.time<-delta.days
+  n.pustules.model.pred.data<-data.frame("n.pustules"=n.pustules.model.new.n.pustules,"temp.days.16.22"=new.temp.days.16.22/delta.days,"temp.16.22.wetness.days"=new.temp.16.22.wetness.days/delta.days)
+  pred.pustule.num.increase<-predict(n.pustules.model,newdata=n.pustules.model.pred.data,re.form=~0)
+  
+  #predict change in plant.inf.intensity from enviro conditions
+  #plant.model.vars<-names(fixef(plant.model))[2:length(names(fixef(plant.model)))]
+  plant.model.new.plant.inf.intens<-.1
+  obs.time<-delta.days
+  plant.model.pred.data<-data.frame("plant.inf.intens"=plant.model.new.plant.inf.intens,"dew.point.days"=new.dew.point.days/delta.days,"temp.7.30.dew.point.days"=new.temp.7.30.dew.point.days/delta.days,"pred.pustule.diam.growth"=pred.pustule.diam.growth,"site"=site)
+  pred.plant.inf.intens.increase<-10^predict(plant.model,newdata=plant.model.pred.data,exclude = 's(site)')
+  
 
   out.data<-data.frame(area=dummy.data,time=rep(delta.days,times=dim),
                              temp.days=rep(new.temp.days,times=dim),temp.days.16.22=rep(new.temp.days.16.22,times=dim),temp.days.7.30=rep(new.temp.days.7.30,times=dim),
                              dew.point.days=rep(new.dew.point.days,times=dim),temp.dew.point.days=rep(new.temp.dew.point.days,times=dim),temp.16.22.dew.point.days=rep(new.temp.16.22.dew.point.days,times=dim),temp.7.30.dew.point.days=rep(new.temp.7.30.dew.point.days,times=dim),
                              wetness.days=rep(new.wetness.days,times=dim),temp.wetness.days=rep(new.temp.wetness.days,times=dim),temp.16.22.wetness.days=rep(new.temp.16.22.wetness.days,times=dim),temp.7.30.wetness.days=rep(new.temp.7.30.wetness.days,times=dim),
-                             tot.rain=rep(new.tot.rain,times=dim),solar.days=rep(new.solar.days,times=dim),wind.speed.days=rep(new.wind.speed.days,times=dim),gust.speed.days=rep(new.gust.speed.days))
+                             tot.rain=rep(new.tot.rain,times=dim),solar.days=rep(new.solar.days,times=dim),wind.speed.days=rep(new.wind.speed.days,times=dim),gust.speed.days=rep(new.gust.speed.days),
+                       pred.pustule.diam.growth=rep(pred.pustule.diam.growth,times=dim),pred.pustule.num.increase=rep(pred.pustule.num.increase,times=dim),pred.plant.inf.intens.increase=rep(pred.plant.inf.intens.increase,times=dim))
   out.data
 }
