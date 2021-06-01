70db1e6bf37c1e8c6552cf87ca081e9ed3af905f
ifmiller <ifmiller@princeton.edu>
2021-06-01 12:17:21 -0400

    switching to gam

--- cross.scale.transmission.dynamics/plant.growth.analysis.R
+++ cross.scale.transmission.dynamics/plant.growth.analysis.R
@@ -4,14 +4,14 @@ library(lmerTest)
 library(progress)
 
 # load data
-source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant.growth.R")
+source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant growth data prep.R")
 
 delta.height<-subset(delta.height,time<=7)
 
 # visualize data
 par(mfrow =c(1,1))
 colors <- ifelse(delta.height$height.next > delta.height$height, "green", "red")
-plot(height.next~height, data = delta.height, col = colors)
+plot(height.next-height~height, data = delta.height, col = colors,xlab="height",ylab="change in heght")
 
 
 ## fit models--only if not already fit
@@ -19,19 +19,19 @@ plot(height.next~height, data = delta.height, col = colors)
 if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/plant.growth.model.RDS"))
 {
   ### construct all combinations of predictors
-  source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant.model.set.creation.R")
+  source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant.growth.model.set.creation.R")
   
   ### create all sets of models
-  model.set <-apply(pred.mat, 1, function(x) as.formula( paste(c("height.next ~ height",predictors[x],'(1|site)'),collapse=" + ")))
+  model.set <-apply(pred.mat, 1, function(x) as.formula( paste(c("height.next ~ s(height)",predictors[x],'s(site,bs="re")'),collapse=" + ")))
   names(model.set)<-seq(1,length(model.set),1)
   
   ## run to search for best  model
   all.fit.models<-c()
-  AIC.benchmark<- AIC(lmer(height.next~height+(1|site),data=delta.height,REML = F)) #cutoff to limit memory usage
+  AIC.benchmark<- AIC(gam(height.next~s(height)+s(site,bs="re"),data=delta.height)) #cutoff to limit memory usage
   pb <- progress_bar$new(total = length(model.set),format = " fitting models [:bar] :percent eta: :eta")
   for (i in 1:length(model.set))
   {
-    suppressMessages(new.mod<-lmer(model.set[[i]],data=delta.height,REML = F))
+    suppressMessages(new.mod<-gam(model.set[[i]],data=delta.height))
     AIC.new.mod<-AIC(new.mod)
     if(AIC.new.mod<=(AIC.benchmark)) {all.fit.models<-append(all.fit.models,list(new.mod))}
     pb$tick()
@@ -43,10 +43,10 @@ if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/
   delta.AICs<-AICs-min(AICs)
   index<-1
   best.model<-all.fit.models[[order(AICs)[index]]]
-  saveRDS(best.model,file="~/Dropbox/flax.rust/cross.scale.transmission.dynamics/models/plant.growth.model.RDS")
+  saveRDS(best.model,file="~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/plant.growth.model.RDS")
 }
 
 ## load best model
 
-plant.growth.model<-readRDS("~/Dropbox/flax.rust/cross.scale.transmission.dynamics/models/plant.growth.model.RDS")
+plant.growth.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/plant.growth.model.RDS")
 
