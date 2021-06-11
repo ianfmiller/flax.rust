20374c6ea1385af90670a5a8c3ecf3a851906bb5
ifmiller <ifmiller@princeton.edu>
2021-06-11 11:35:54 -0600

    get rid of unneeded weath data

--- cross.scale.transmission.dynamics/predict plant height change funcs.R
+++ cross.scale.transmission.dynamics/predict plant height change funcs.R
@@ -18,33 +18,17 @@ predict.plant.growth<-function(height.last,site,date0,date1,exclude.site=T)
   temp.rh.sub<-subset(temp.rh.sub,!is.na(temp.c)) #### throw out NAs
   temp.rh.sub<-cbind(temp.rh.sub,interval.length=c(diff(as.numeric(temp.rh.sub$date.time))/(60*60*24),NA)) #add interval length in days
   
-  ## subset weather data to relevant window
-  weath.sub<-all.weath[which(all.weath$site==site),] #pull out weath data for site
-  weath.sub<-subset(weath.sub,date<=date1) #### pull out relevant data
-  weath.sub<-subset(weath.sub,date>=date0) #### pull out relevant data
-  weath.sub<-cbind(weath.sub,interval.length=c(diff(as.numeric(weath.sub$date))/(60*60*24),NA))
-  
   # calculate environmental variable metrics
   new.temp.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$interval.length,na.rm = T) #temperature days
   new.temp.days.16.22<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T) #time (in days) during which temp between 16 and 22 celsius
   new.temp.days.7.30<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T) #time (in days) during which temp between 7 and 30 celsius
   new.dew.point.days<-sum(temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T) #Dew point days
   
-  #calculate weather metrics
-  new.wetness.days<-sum(weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.tot.rain<-sum(weath.sub$rain,na.rm=T)
-  new.solar.days<-sum(weath.sub$solar.radiation*weath.sub$interval.length,na.rm = T)
-  new.wind.speed.days<-sum(weath.sub$wind.speed*weath.sub$interval.length,na.rm = T)
-  new.gust.speed.days<-sum(weath.sub$wind.direction*weath.sub$interval.length,na.rm = T)
-  
   #calculate joint environmental variable metrics--accounts for temporal co-occurence of environmental variables
   new.temp.dew.point.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T)
   new.temp.16.22.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T)
   new.temp.7.30.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T)
-  new.temp.wetness.days<-sum(weath.sub$temp*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.temp.16.22.wetness.days<-sum(weath.sub$temp.16.22*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.temp.7.30.wetness.days<-sum(weath.sub$temp.7.30*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  
+
   delta.days<-as.numeric(date1-date0)
   
   # make forward prediction
@@ -65,33 +49,17 @@ predict.plant.growth.boot<-function(height.last,site,date0,date1)
   temp.rh.sub<-subset(temp.rh.sub,!is.na(temp.c)) #### throw out NAs
   temp.rh.sub<-cbind(temp.rh.sub,interval.length=c(diff(as.numeric(temp.rh.sub$date.time))/(60*60*24),NA)) #add interval length in days
   
-  ## subset weather data to relevant window
-  weath.sub<-all.weath[which(all.weath$site==site),] #pull out weath data for site
-  weath.sub<-subset(weath.sub,date<=date1) #### pull out relevant data
-  weath.sub<-subset(weath.sub,date>=date0) #### pull out relevant data
-  weath.sub<-cbind(weath.sub,interval.length=c(diff(as.numeric(weath.sub$date))/(60*60*24),NA))
-  
   # calculate environmental variable metrics
   new.temp.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$interval.length,na.rm = T) #temperature days
   new.temp.days.16.22<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T) #time (in days) during which temp between 16 and 22 celsius
   new.temp.days.7.30<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T) #time (in days) during which temp between 7 and 30 celsius
   new.dew.point.days<-sum(temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T) #Dew point days
   
-  #calculate weather metrics
-  new.wetness.days<-sum(weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.tot.rain<-sum(weath.sub$rain,na.rm=T)
-  new.solar.days<-sum(weath.sub$solar.radiation*weath.sub$interval.length,na.rm = T)
-  new.wind.speed.days<-sum(weath.sub$wind.speed*weath.sub$interval.length,na.rm = T)
-  new.gust.speed.days<-sum(weath.sub$wind.direction*weath.sub$interval.length,na.rm = T)
-  
   #calculate joint environmental variable metrics--accounts for temporal co-occurence of environmental variables
   new.temp.dew.point.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T)
   new.temp.16.22.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T)
   new.temp.7.30.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T)
-  new.temp.wetness.days<-sum(weath.sub$temp*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.temp.16.22.wetness.days<-sum(weath.sub$temp.16.22*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.temp.7.30.wetness.days<-sum(weath.sub$temp.7.30*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  
+
   delta.days<-as.numeric(date1-date0)
   
   # make forward prediction
@@ -112,7 +80,7 @@ predict.plant.growth.boot<-function(height.last,site,date0,date1)
 }
 
 #function for predicting plant height change
-predict.plant.growth.last<-function(height.next,site,date0,date1)
+predict.plant.growth.last<-function(height.next,site,date0,date1,exclude.site=T)
 {
   # load weather data
   ## subst temp rh data to relevant window
@@ -122,33 +90,17 @@ predict.plant.growth.last<-function(height.next,site,date0,date1)
   temp.rh.sub<-subset(temp.rh.sub,!is.na(temp.c)) #### throw out NAs
   temp.rh.sub<-cbind(temp.rh.sub,interval.length=c(diff(as.numeric(temp.rh.sub$date.time))/(60*60*24),NA)) #add interval length in days
   
-  ## subset weather data to relevant window
-  weath.sub<-all.weath[which(all.weath$site==site),] #pull out weath data for site
-  weath.sub<-subset(weath.sub,date<=date1) #### pull out relevant data
-  weath.sub<-subset(weath.sub,date>=date0) #### pull out relevant data
-  weath.sub<-cbind(weath.sub,interval.length=c(diff(as.numeric(weath.sub$date))/(60*60*24),NA))
-  
   # calculate environmental variable metrics
   new.temp.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$interval.length,na.rm = T) #temperature days
   new.temp.days.16.22<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T) #time (in days) during which temp between 16 and 22 celsius
   new.temp.days.7.30<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T) #time (in days) during which temp between 7 and 30 celsius
   new.dew.point.days<-sum(temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T) #Dew point days
   
-  #calculate weather metrics
-  new.wetness.days<-sum(weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.tot.rain<-sum(weath.sub$rain,na.rm=T)
-  new.solar.days<-sum(weath.sub$solar.radiation*weath.sub$interval.length,na.rm = T)
-  new.wind.speed.days<-sum(weath.sub$wind.speed*weath.sub$interval.length,na.rm = T)
-  new.gust.speed.days<-sum(weath.sub$wind.direction*weath.sub$interval.length,na.rm = T)
-  
   #calculate joint environmental variable metrics--accounts for temporal co-occurence of environmental variables
   new.temp.dew.point.days<-sum(temp.rh.sub$temp.c*temp.rh.sub$dew.pt.c*temp.rh.sub$interval.length,na.rm = T)
   new.temp.16.22.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,16,22)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,16,22)$interval.length,na.rm = T)
   new.temp.7.30.dew.point.days<-sum(1*temp.rh.sub.func(temp.rh.sub,7,30)$dew.pt.c*temp.rh.sub.func(temp.rh.sub,7,30)$interval.length,na.rm = T)
-  new.temp.wetness.days<-sum(weath.sub$temp*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.temp.16.22.wetness.days<-sum(weath.sub$temp.16.22*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  new.temp.7.30.wetness.days<-sum(weath.sub$temp.7.30*weath.sub$wetness*weath.sub$interval.length,na.rm = T)
-  
+
   delta.days<-as.numeric(date1-date0)
   
   # make backwards prediction
@@ -156,7 +108,7 @@ predict.plant.growth.last<-function(height.next,site,date0,date1)
   {
     plant.height.last.test<-x
     pred.data<-data.frame("height"=plant.height.last.test,"dew.point.days"=new.dew.point.days,"temp.dew.point.days"=new.temp.dew.point.days,"site"=site)
-    plant.height.next.pred<-predict(plant.growth.model,newdata=pred.data,exclude = 's(site)')
+    if(exclude.site) {plant.height.next.pred<-predict(plant.growth.model,newdata=pred.data,exclude = 's(site)')} else {plant.height.next.pred<-predict(plant.growth.model,newdata=pred.data)}
     abs(plant.height.next.pred-plant.height.next)
   }
   plant.height.last<-optim(c(plant.height.next),pred.func,method = "Brent",lower=0,upper=10e6)$par
