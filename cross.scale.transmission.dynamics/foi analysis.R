05b7195123337cc5bc5329d7c4e2992c04f2b641
ifmiller <ifmiller@princeton.edu>
2021-07-12 09:15:47 -0600

    update foi model fitting

--- cross.scale.transmission.dynamics/foi analysis.R
+++ cross.scale.transmission.dynamics/foi analysis.R
@@ -57,6 +57,30 @@ axis(1,cex.axis=1.5)
 
 if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS"))
 {
+  source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/foi model set creation.R") ### load models
+  
+  model.set <-apply(pred.mat, 1, function(x) as.formula( paste(c("status.next ~ foi",predictors[x]),collapse=" + ")))
+  names(model.set)<-seq(1,length(model.set),1)
+  
+  ## run to search for best  model
+  all.fit.models<-c()
+  AIC.benchmark<- AIC(glm(status.next~foi,data = foi.data)) #cutoff to limit memory usage
+  #AIC.benchmark<- 8025
+  pb <- progress_bar$new(total = length(model.set),format = " fitting models [:bar] :percent eta: :eta")
+  for (i in 1:length(model.set))
+  {
+    suppressMessages(new.mod<-glm(model.set[[i]],data=foi.data))
+    AIC.new.mod<-AIC(new.mod)
+    if(AIC.new.mod<=(AIC.benchmark)) {all.fit.models<-append(all.fit.models,list(new.mod))}
+    pb$tick()
+  }
+  
+  ## compare between models
+  AICs<-unlist(lapply(all.fit.models,AIC))
+  delta.AICs<-AICs-min(AICs)
+  index<-1
+  best.model<-all.fit.models[[order(AICs)[index]]]
+  
   ### create all sets of models--Only use foi and first obs height due to low sample size for environmental vars within several sites
   mod0<-glm(status.next~foi,data=foi.data,family = "binomial")
   mod1<-glm(status.next~foi+height.cm,data=foi.data,family = "binomial") 
@@ -75,6 +99,14 @@ axis(2,cex.axis=1.5,col="red",col.axis="red")
 mtext("odds of infection",side=2,line=2.5,cex=1.5,col="red")
 axis(1,cex.axis=1.5)
 
-new.data<-data.frame("foi"=seq(0,.15,.0001))
+fois<-seq(0,.15,.0001)
+new.data<-data.frame("foi"=fois,
+                     "mean.temp.days.16.22"=rep(mean(foi.data$mean.temp.days.16.22),times=length(fois)),
+                     "mean.dew.point.days"=rep(mean(foi.data$mean.dew.point.days),times=length(fois)),
+                     "mean.temp.dew.point.days"=rep(mean(foi.data$mean.temp.dew.point.days ),times=length(fois)),
+                     "mean.temp.7.30.wetness.days"=rep(mean(foi.data$mean.temp.7.30.wetness.days),times=length(fois)),
+                     "mean.tot.rain"=rep(mean(foi.data$mean.tot.rain ),times=length(fois))
+                     )
+
 points(new.data$foi,predict(best.model,newdata = new.data,type = "response"),type="l",col="red",lwd=4,lty=1)
 
