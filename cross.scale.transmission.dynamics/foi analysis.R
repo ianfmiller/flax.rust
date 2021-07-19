d481c528937f52819585ff839f06b466f7175e9a
ifmiller <ifmiller@princeton.edu>
2021-07-19 12:04:13 -0600

    foi model u+ vis update

--- cross.scale.transmission.dynamics/foi analysis.R
+++ cross.scale.transmission.dynamics/foi analysis.R
@@ -89,28 +89,42 @@ if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/
 best.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
 
 par(mfrow=c(1,1),mar=c(6,8,6,6))
-plot(jitter(foi.data$foi),jitter(foi.data$status.next),xlab="predicted spore deposition",ylab="",axes=F,cex.lab=1.5,ylim=c(0,.1))
+plot(foi.data$foi,jitter(foi.data$status.next,factor=.5),xlab="predicted spore deposition",ylab="",axes=F,cex.lab=1.5,ylim=c(-.2,1.2))
 axis(2,at=c(0,1),labels = c("healthy","infected"),cex.axis=1.5,line=3,tick = F)
-axis(2,cex.axis=1.5,col="red",col.axis="red")
+axis(2,at=c(0,.25,.5,.75,1),cex.axis=1.5,col="red",col.axis="red")
 mtext("odds of infection",side=2,line=2.5,cex=1.5,col="red")
 axis(1,cex.axis=1.5)
 
 fois<-seq(0,.15,.0001)
-new.data<-data.frame("foi"=fois,
-                     "height.cm"=rep(quantile(foi.data$height.cm,.5),times=length(fois)),
-                     "mean.temp.days.7.30"=rep(mean(foi.data$mean.temp.days.7.30),times=length(fois)),
-                     "mean.dew.point.days"=rep(mean(foi.data$mean.dew.point.days),times=length(fois)),
-                     "mean.temp.7.30.dew.point.days"=rep(mean(foi.data$mean.temp.7.30.dew.point.days),times=length(fois)),
-                     "mean.wetness.days"=rep(mean(foi.data$mean.wetness.days),times=length(fois)),
-                     "mean.temp.wetness.days"=rep(quantile(foi.data$mean.temp.wetness.days,.5),times=length(fois)),
-                     "pred.pustule.diam.growth"=rep(mean(foi.data$pred.pustule.diam.growth ),times=length(fois)),
-                     "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)),
-                     "pred.plant.inf.intens.increase"=rep(mean(foi.data$pred.plant.inf.intens.increase ),times=length(fois)),
-                     "mean.temp.days.16.22"=rep(mean(foi.data$mean.temp.days.16.22),times=length(fois)),
-                     "mean.temp.7.30.dew.point.days"=rep(quantile(foi.data$mean.temp.7.30.dew.point.days,.5),times=length(fois)),
-                     "delta.days"=rep(mean(foi.data$delta.days),times=length(fois))
-                     )
+new.data.1<-data.frame("foi"=fois,
+                     "height.cm"=5,
+                     "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)))
+new.data.2<-data.frame("foi"=fois,
+                       "height.cm"=10,
+                       "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)))
+new.data.3<-data.frame("foi"=fois,
+                       "height.cm"=25,
+                       "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)))
+new.data.4<-data.frame("foi"=fois,
+                       "height.cm"=50,
+                       "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)))
 
-points(new.data$foi,predict(best.model,newdata = new.data,type = "response",re.form=NA),type="l",col="red",lwd=1,lty=1)
-plot(best.model$data$foi,predict(best.model,type="response"))
+points(new.data.1$foi,predict(best.model,newdata = new.data.1,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=1)
+points(new.data.2$foi,predict(best.model,newdata = new.data.2,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=2)
+points(new.data.3$foi,predict(best.model,newdata = new.data.3,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=3)
+points(new.data.4$foi,predict(best.model,newdata = new.data.4,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=4)
+legend("topright",legend=c("50cm","25cm","10cm","5cm"),col="red",lty=c(4,3,2,1),bty="n",cex=1.5,lwd=2)
 
+par(mfrow=c(1,1),mar=c(6,8,6,6))
+plot(0,0,type = "n",xlab="predicted spore deposition",ylab="relative odds of infection",axes=F,cex.lab=1.5,ylim=c(0,10),xlim = c(0,.1))
+axis(1)
+axis(2)
+base.pred<-predict(best.model,newdata = new.data.1,type = "response",re.form=NA)[1]
+points(new.data.1$foi,predict(best.model,newdata = new.data.1,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=1)
+points(new.data.2$foi,predict(best.model,newdata = new.data.2,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=2)
+points(new.data.3$foi,predict(best.model,newdata = new.data.3,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=3)
+points(new.data.4$foi,predict(best.model,newdata = new.data.4,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=4)
+legend("right",legend=c("50cm","25cm","10cm","5cm"),col="red",lty=c(4,3,2,1),bty="n",cex=1.5,lwd=2)
+
+
+         
