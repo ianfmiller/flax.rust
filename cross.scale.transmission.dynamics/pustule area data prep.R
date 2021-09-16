792bfb23ab7b4bfaa8826e31c9c13554e02b0d1e
ifmiller <ifmiller@princeton.edu>
2021-09-16 14:59:34 -0400

    comment out vpd

--- cross.scale.transmission.dynamics/pustule area data prep.R
+++ cross.scale.transmission.dynamics/pustule area data prep.R
@@ -37,9 +37,9 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
   mean.abs.hum<-c() #absolute humidity
   max.abs.hum<-c()
   min.abs.hum<-c()
-  mean.vpd<-c() #vapor pressure deficit
-  max.vpd<-c() 
-  min.vpd<-c() 
+  #mean.vpd<-c() #vapor pressure deficit
+  #max.vpd<-c() 
+  #min.vpd<-c() 
   mean.wetness<-c()
   tot.rain<-c()
   mean.solar<-c()
@@ -100,13 +100,13 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
               new.max.abs.hum<-max(abs.hum,na.rm=T)
               new.min.abs.hum<-min(abs.hum,na.rm=T)
               
-              svps<- 0.6108 * exp(17.27 * temp.rh.sub$temp.c / (temp.rh.sub$temp.c + 237.3)) #saturation vapor pressures
-              avps<- temp.rh.sub$rh / 100 * svps #actual vapor pressures 
-              vpds<-avps-svps
+              #svps<- 0.6108 * exp(17.27 * temp.rh.sub$temp.c / (temp.rh.sub$temp.c + 237.3)) #saturation vapor pressures
+              #avps<- temp.rh.sub$rh / 100 * svps #actual vapor pressures 
+              #vpds<-avps-svps
               
-              new.mean.vpd<-mean(vpds,na.rm=T)
-              new.max.vpd<-max(vpds,na.rm=T)
-              new.min.vpd<-min(vpds,na.rm=T)
+              #new.mean.vpd<-mean(vpds,na.rm=T)
+              #new.max.vpd<-max(vpds,na.rm=T)
+              #new.min.vpd<-min(vpds,na.rm=T)
               
               new.mean.wetness<-mean(weath.sub$wetness,na.rm = T)
               new.tot.rain<-sum(weath.sub$rain,na.rm=T)
@@ -134,9 +134,9 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
               mean.abs.hum<-c(mean.abs.hum,new.mean.abs.hum)
               max.abs.hum<-c(max.abs.hum,new.max.abs.hum)
               min.abs.hum<-c(min.abs.hum,new.min.abs.hum)
-              mean.vpd<-c(mean.vpd,new.mean.vpd)
-              max.vpd<-c(max.vpd,new.max.vpd)
-              min.vpd<-c(min.vpd,new.min.vpd)
+              #mean.vpd<-c(mean.vpd,new.mean.vpd)
+              #max.vpd<-c(max.vpd,new.max.vpd)
+              #min.vpd<-c(min.vpd,new.min.vpd)
               mean.wetness<-c(mean.wetness,new.mean.wetness)
               tot.rain<-c(tot.rain,new.tot.rain)
               mean.solar<-c(mean.solar,new.mean.solar)
@@ -151,7 +151,7 @@ if(!(file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics
   delta.pustules<-data.frame(tag=factor(tags),site=factor(sites),stem.iter=stem.iters,leaf.iter=leaf.iters,pustule.num=pustule.nums,area=start.vals,area.next=end.vals,time=days,
                              mean.temp=mean.temp,max.temp=max.temp,min.temp=min.temp,
                              mean.abs.hum=mean.abs.hum,max.abs.hum=max.abs.hum,min.abs.hum=min.abs.hum,
-                             mean.vpd=mean.vpd,max.vpd=max.vpd,min.vpd=min.vpd,
+                             #mean.vpd=mean.vpd,max.vpd=max.vpd,min.vpd=min.vpd,
                              mean.wetness=mean.wetness,tot.rain=tot.rain,mean.solar=mean.solar)
   
   saveRDS(pustules,file="~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/summarized data/pustules.RDS")
