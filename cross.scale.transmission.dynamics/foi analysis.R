09753ee2c9fef2438ecb9197251cf4041a9bbcfb
ifmiller <ifmiller@princeton.edu>
2021-09-29 20:51:54 -0400

    trying a new method of foi model fitting. If it works will need to come back and clean up these files

--- cross.scale.transmission.dynamics/foi analysis.R
+++ cross.scale.transmission.dynamics/foi analysis.R
@@ -56,12 +56,30 @@ axis(1,cex.axis=1.5)
 
 if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS"))
 {
+  mod0<-glmer(status.next~
+                log10(foi)+
+                #height.cm+
+                #mean.temp + 
+                #max.temp + 
+                #min.temp + 
+                #mean.abs.hum + 
+                #max.abs.hum + 
+                #min.abs.hum + 
+                #tot.rain + 
+                #mean.solar + 
+                (1|site),
+              data = foi.data,
+              family = binomial(link="logit")
+              )
+  summary(mod0)
+  
+  
   source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/foi model set creation.R") ### load models
-  model.set <-apply(pred.mat, 1, function(x) as.formula( paste(c("status.next ~ foi + (1|site)",predictors[x]),collapse=" + ")))
+  model.set <-apply(pred.mat, 1, function(x) as.formula( paste(c("status.next ~  log10(foi)*height.cm + (1|site)",predictors[x]),collapse=" + ")))
   names(model.set)<-seq(1,length(model.set),1)
   ## run to search for best  model
   all.fit.models<-c()
-  AIC.benchmark<- AIC(glmer(status.next~foi + (1|site),data = foi.data,family = binomial(link="logit"),nAGQ = 0)) #cutoff to limit memory usage
+  AIC.benchmark<- AIC(glmer(status.next~log10(foi)*height.cm + (1|site),data = foi.data,family = binomial(link="logit"),nAGQ = 0)) #cutoff to limit memory usage
   pb <- progress_bar$new(total = length(model.set),format = " fitting models [:bar] :percent eta: :eta")
   for (i in 1:length(model.set))
   {
@@ -78,74 +96,65 @@ if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/
   best.model<-all.fit.models[[order(AICs)[index]]]
   summary(best.model)
   saveRDS(best.model,file="~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
-  #mod0<-glm(status.next~log10(foi),data=foi.data,family = "binomial")
-  #mod1<-glm(status.next~log10(foi)+height.cm,data=foi.data,family = "binomial") 
-  #mod2<-glm(status.next~log10(foi)*height.cm,data=foi.data,family = "binomial")
-  #AIC(mod0,mod1,mod2)
-  #saveRDS(mod0,file="~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
 }
 
-best.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
+foi.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
 
 par(mfrow=c(1,1),mar=c(6,8,6,6))
-plot(foi.data$foi,jitter(foi.data$status.next,factor=.5),xlab="predicted spore deposition",ylab="",axes=F,cex.lab=1.5,ylim=c(-.2,1.2))
+plot(log10(foi.data$foi),jitter(foi.data$status.next,factor=.5),xlab="predicted spore deposition",ylab="",axes=F,cex.lab=1.5,ylim=c(-.2,1.2),xlim=c(-8,1))
 axis(2,at=c(0,1),labels = c("healthy","infected"),cex.axis=1.5,line=3,tick = F)
 axis(2,at=c(0,.25,.5,.75,1),cex.axis=1.5,col="red",col.axis="red")
 mtext("odds of infection",side=2,line=2.5,cex=1.5,col="red")
 axis(1,cex.axis=1.5)
 
-fois<-seq(0,1.5,.01)
+fois<-10^seq(-8,0,.25)
 new.data.1<-data.frame("foi"=fois,
                      "height.cm"=5,
                      "mean.temp"=mean(foi.data$mean.temp),
                      "max.temp"=mean(foi.data$max.temp),
                      "min.temp"=mean(foi.data$min.temp),
-                     "pred.pustule.diam.growth"=rep(mean(foi.data$pred.pustule.diam.growth ),times=length(fois)),
-                     "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)),
-                     "pred.plant.inf.intens.increase"=rep(mean(foi.data$pred.plant.inf.intens.increase ),times=length(fois))
+                     "mean.abs.hum"=mean(foi.data$mean.abs.hum),
+                     "tot.rain"=mean(foi.data$tot.rain)
                      )
 new.data.2<-data.frame("foi"=fois,
                        "height.cm"=10,
                        "mean.temp"=mean(foi.data$mean.temp),
                        "max.temp"=mean(foi.data$max.temp),
                        "min.temp"=mean(foi.data$min.temp),
-                       "pred.pustule.diam.growth"=rep(mean(foi.data$pred.pustule.diam.growth ),times=length(fois)),
-                       "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)),
-                       "pred.plant.inf.intens.increase"=rep(mean(foi.data$pred.plant.inf.intens.increase ),times=length(fois))
+                       "mean.abs.hum"=mean(foi.data$mean.abs.hum),
+                       "tot.rain"=mean(foi.data$tot.rain)
                       )
 new.data.3<-data.frame("foi"=fois,
                        "height.cm"=25,
                        "mean.temp"=mean(foi.data$mean.temp),
                        "max.temp"=mean(foi.data$max.temp),
                        "min.temp"=mean(foi.data$min.temp),
-                       "pred.pustule.diam.growth"=rep(mean(foi.data$pred.pustule.diam.growth ),times=length(fois)),
-                       "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)),
-                       "pred.plant.inf.intens.increase"=rep(mean(foi.data$pred.plant.inf.intens.increase ),times=length(fois))
+                       "mean.abs.hum"=mean(foi.data$mean.abs.hum),
+                       "tot.rain"=mean(foi.data$tot.rain)
                       )
 new.data.4<-data.frame("foi"=fois,
                        "height.cm"=50,
                        "mean.temp"=mean(foi.data$mean.temp),
                        "max.temp"=mean(foi.data$max.temp),
                        "min.temp"=mean(foi.data$min.temp),
-                       "pred.pustule.diam.growth"=rep(mean(foi.data$pred.pustule.diam.growth ),times=length(fois)),
-                       "pred.pustule.num.increase"=rep(mean(foi.data$pred.pustule.num.increase ),times=length(fois)),
-                       "pred.plant.inf.intens.increase"=rep(mean(foi.data$pred.plant.inf.intens.increase ),times=length(fois))
+                       "mean.abs.hum"=mean(foi.data$mean.abs.hum),
+                       "tot.rain"=mean(foi.data$tot.rain)
                       )
-points(new.data.1$foi,predict(best.model,newdata = new.data.1,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=1)
-points(new.data.2$foi,predict(best.model,newdata = new.data.2,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=2)
-points(new.data.3$foi,predict(best.model,newdata = new.data.3,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=3)
-points(new.data.4$foi,predict(best.model,newdata = new.data.4,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=4)
+points(log10(new.data.1$foi),predict(foi.model,newdata = new.data.1,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=1)
+points(log10(new.data.2$foi),predict(foi.model,newdata = new.data.2,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=2)
+points(log10(new.data.3$foi),predict(foi.model,newdata = new.data.3,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=3)
+points(log10(new.data.4$foi),predict(foi.model,newdata = new.data.4,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=4)
 legend("topright",legend=c("50cm","25cm","10cm","5cm"),col="red",lty=c(4,3,2,1),bty="n",cex=1.5,lwd=2)
 
 par(mfrow=c(1,1),mar=c(6,8,6,6))
-plot(0,0,type = "n",xlab="predicted spore deposition",ylab="relative odds of infection",axes=F,cex.lab=1.5,ylim=c(0,10),xlim = c(0,.1))
+plot(0,0,type = "n",xlab="predicted spore deposition",ylab="relative odds of infection",axes=F,cex.lab=1.5,ylim=c(0,10),xlim = c(-8,1))
 axis(1)
 axis(2)
-base.pred<-predict(best.model,newdata = new.data.1,type = "response",re.form=NA)[1]
-points(new.data.1$foi,predict(best.model,newdata = new.data.1,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=1)
-points(new.data.2$foi,predict(best.model,newdata = new.data.2,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=2)
-points(new.data.3$foi,predict(best.model,newdata = new.data.3,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=3)
-points(new.data.4$foi,predict(best.model,newdata = new.data.4,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=4)
+base.pred<-predict(foi.model,newdata = new.data.1,type = "response",re.form=NA)[1]
+points(log10(new.data.1$foi),predict(foi.model,newdata = new.data.1,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=1)
+points(log10(new.data.2$foi),predict(foi.model,newdata = new.data.2,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=2)
+points(log10(new.data.3$foi),predict(foi.model,newdata = new.data.3,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=3)
+points(log10(new.data.4$foi),predict(foi.model,newdata = new.data.4,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=2,lty=4)
 legend("topleft",legend=c("50cm","25cm","10cm","5cm"),col="red",lty=c(4,3,2,1),bty="n",cex=1.5,lwd=2)
 
 
