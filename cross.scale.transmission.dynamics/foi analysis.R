caba9d0cb39f5cbbeac4988b782ed7fc0991a4ed
ifmiller <ifmiller@princeton.edu>
2021-10-01 18:01:54 -0400

    refit foi model

--- cross.scale.transmission.dynamics/foi analysis.R
+++ cross.scale.transmission.dynamics/foi analysis.R
@@ -6,6 +6,7 @@ library(progress)
 
 source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/building foi dataset.R")
 foi.data<-foi.data[which(foi.data$status==0),]
+foi.data$site<-as.factor(foi.data$site)
 
 # visualize data
 
@@ -56,65 +57,79 @@ axis(1,cex.axis=1.5)
 
 if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS"))
 {
-  mod0<-glmer(status.next~
-                log10(foi)+
-                #height.cm+
-                #mean.temp + 
-                #max.temp + 
-                #min.temp + 
-                #mean.abs.hum + 
-                #max.abs.hum + 
-                #min.abs.hum + 
-                #tot.rain + 
-                #mean.solar + 
-                (1|site),
-              data = foi.data,
-              family = binomial(link="logit")
-              )
-  summary(mod0)
+
+  mod0<-gam(status.next~
+            log10(foi)+
+            s(height.cm,bs="cs",k=4) +
+            s(mean.temp,bs="cs",k=4) + 
+            s(max.temp,bs="cs",k=4) + 
+            s(min.temp,bs="cs",k=4) + 
+            s(mean.abs.hum,bs="cs",k=4) + 
+            s(max.abs.hum,bs="cs",k=4) + 
+            s(min.abs.hum,bs="cs",k=4) + 
+            s(tot.rain,bs="cs",k=4) + 
+            s(mean.solar,bs="cs",k=4)+
+            s(site,bs="re",k=4),
+            data = foi.data,
+            family = binomial()
+  )
+  summary(mod0) #indicates all but log10(foi), height.cm are insignificant
   
+  mod1<-gam(status.next~
+              log10(foi)+
+              s(height.cm,bs="cs",k=4)+
+              #s(mean.temp,bs="cs",k=4) + 
+              #s(max.temp,bs="cs",k=4) + 
+              #s(min.temp,bs="cs",k=4) + 
+              #s(mean.abs.hum,bs="cs",k=4) + 
+              #s(max.abs.hum,bs="cs",k=4) + 
+              #s(min.abs.hum,bs="cs",k=4) + 
+              #s(tot.rain,bs="cs",k=4) + 
+              #s(mean.solar,bs="cs",k=4)+
+              s(site,bs="re",k=4),
+            data = foi.data,
+            family = binomial()
+  )
+  summary(mod1) #indicates that height.cm is no longer significant
   
-  source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/foi model set creation.R") ### load models
-  model.set <-apply(pred.mat, 1, function(x) as.formula( paste(c("status.next ~  log10(foi)*height.cm + (1|site)",predictors[x]),collapse=" + ")))
-  names(model.set)<-seq(1,length(model.set),1)
-  ## run to search for best  model
-  all.fit.models<-c()
-  AIC.benchmark<- AIC(glmer(status.next~log10(foi)*height.cm + (1|site),data = foi.data,family = binomial(link="logit"),nAGQ = 0)) #cutoff to limit memory usage
-  pb <- progress_bar$new(total = length(model.set),format = " fitting models [:bar] :percent eta: :eta")
-  for (i in 1:length(model.set))
-  {
-    suppressMessages(new.mod<-glmer(model.set[[i]],data=foi.data,family=binomial,nAGQ=0))
-    AIC.new.mod<-AIC(new.mod)
-    if(AIC.new.mod<=(AIC.benchmark)) {all.fit.models<-append(all.fit.models,list(new.mod))}
-    pb$tick()
-  }
+  mod2<-gam(status.next~
+            log10(foi)+
+            #s(height.cm,bs="cs",k=4),
+            #s(mean.temp,bs="cs",k=4) + 
+            #s(max.temp,bs="cs",k=4) + 
+            #s(min.temp,bs="cs",k=4) + 
+            #s(mean.abs.hum,bs="cs",k=4) + 
+            #s(max.abs.hum,bs="cs",k=4) + 
+            #s(min.abs.hum,bs="cs",k=4) + 
+            #s(tot.rain,bs="cs",k=4) + 
+            #s(mean.solar,bs="cs",k=4)+
+            s(site,bs="re",k=4),
+            data = foi.data,
+            family = binomial()
+  ) # all predictors now significant
+  summary(mod2)
   
-  ## compare between models
-  AICs<-unlist(lapply(all.fit.models,AIC))
-  delta.AICs<-AICs-min(AICs)
-  index<-1
-  best.model<-all.fit.models[[order(AICs)[index]]]
-  summary(best.model)
-  saveRDS(best.model,file="~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
+  saveRDS(mod2,file="~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
 }
 
 foi.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/foi.model.RDS")
 
 par(mfrow=c(1,1),mar=c(6,8,6,6))
-plot(log10(foi.data$foi),jitter(foi.data$status.next,factor=.5),xlab="predicted spore deposition",ylab="",axes=F,cex.lab=1.5,ylim=c(-.2,1.2),xlim=c(-8,1))
+plot(log10(foi.data$foi),jitter(foi.data$status.next,factor=.5),xlab="predicted spore deposition",ylab="",axes=F,cex.lab=1.5,ylim=c(-.2,1.2),xlim=c(-6,3))
 axis(2,at=c(0,1),labels = c("healthy","infected"),cex.axis=1.5,line=3,tick = F)
 axis(2,at=c(0,.25,.5,.75,1),cex.axis=1.5,col="red",col.axis="red")
 mtext("odds of infection",side=2,line=2.5,cex=1.5,col="red")
 axis(1,cex.axis=1.5)
 
-fois<-10^seq(-8,0,.25)
+fois<-10^seq(-6,3,.25)
 new.data.1<-data.frame("foi"=fois,
                      "height.cm"=5,
                      "mean.temp"=mean(foi.data$mean.temp),
                      "max.temp"=mean(foi.data$max.temp),
                      "min.temp"=mean(foi.data$min.temp),
                      "mean.abs.hum"=mean(foi.data$mean.abs.hum),
-                     "tot.rain"=mean(foi.data$tot.rain)
+                     "tot.rain"=mean(foi.data$tot.rain),
+                     "site"="CC"
                      )
 new.data.2<-data.frame("foi"=fois,
                        "height.cm"=10,
@@ -122,7 +137,8 @@ new.data.2<-data.frame("foi"=fois,
                        "max.temp"=mean(foi.data$max.temp),
                        "min.temp"=mean(foi.data$min.temp),
                        "mean.abs.hum"=mean(foi.data$mean.abs.hum),
-                       "tot.rain"=mean(foi.data$tot.rain)
+                       "tot.rain"=mean(foi.data$tot.rain),
+                       "site"="CC"
                       )
 new.data.3<-data.frame("foi"=fois,
                        "height.cm"=25,
@@ -130,7 +146,8 @@ new.data.3<-data.frame("foi"=fois,
                        "max.temp"=mean(foi.data$max.temp),
                        "min.temp"=mean(foi.data$min.temp),
                        "mean.abs.hum"=mean(foi.data$mean.abs.hum),
-                       "tot.rain"=mean(foi.data$tot.rain)
+                       "tot.rain"=mean(foi.data$tot.rain),
+                       "site"="CC"
                       )
 new.data.4<-data.frame("foi"=fois,
                        "height.cm"=50,
@@ -138,16 +155,17 @@ new.data.4<-data.frame("foi"=fois,
                        "max.temp"=mean(foi.data$max.temp),
                        "min.temp"=mean(foi.data$min.temp),
                        "mean.abs.hum"=mean(foi.data$mean.abs.hum),
-                       "tot.rain"=mean(foi.data$tot.rain)
+                       "tot.rain"=mean(foi.data$tot.rain),
+                       "site"="CC"
                       )
-points(log10(new.data.1$foi),predict(foi.model,newdata = new.data.1,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=1)
-points(log10(new.data.2$foi),predict(foi.model,newdata = new.data.2,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=2)
-points(log10(new.data.3$foi),predict(foi.model,newdata = new.data.3,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=3)
-points(log10(new.data.4$foi),predict(foi.model,newdata = new.data.4,type = "response",re.form=NA),type="l",col="red",lwd=2,lty=4)
+points(log10(new.data.1$foi),predict(foi.model,newdata = new.data.1,type = "response",exclude = "s(site)"),type="l",col="red",lwd=2,lty=1)
+points(log10(new.data.2$foi),predict(foi.model,newdata = new.data.2,type = "response",exclude = "s(site)"),type="l",col="red",lwd=2,lty=2)
+points(log10(new.data.3$foi),predict(foi.model,newdata = new.data.3,type = "response",exclude = "s(site)"),type="l",col="red",lwd=2,lty=3)
+points(log10(new.data.4$foi),predict(foi.model,newdata = new.data.4,type = "response",exclude = "s(site)"),type="l",col="red",lwd=2,lty=4)
 legend("topright",legend=c("50cm","25cm","10cm","5cm"),col="red",lty=c(4,3,2,1),bty="n",cex=1.5,lwd=2)
 
 par(mfrow=c(1,1),mar=c(6,8,6,6))
-plot(0,0,type = "n",xlab="predicted spore deposition",ylab="relative odds of infection",axes=F,cex.lab=1.5,ylim=c(0,10),xlim = c(-8,1))
+plot(0,0,type = "n",xlab="predicted spore deposition",ylab="relative odds of infection",axes=F,cex.lab=1.5,ylim=c(0,100),xlim = c(-6,3))
 axis(1)
 axis(2)
 base.pred<-predict(foi.model,newdata = new.data.1,type = "response",re.form=NA)[1]
