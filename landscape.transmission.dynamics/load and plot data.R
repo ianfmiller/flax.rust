4b1db631908e9008f13748a0f88fda5e3a24a903
ifmiller <ifmiller@princeton.edu>
2021-10-15 11:43:57 -0400

    working towards loop for extracting data and plotting

--- landscape.transmission.dynamics/load and plot data.R
+++ landscape.transmission.dynamics/load and plot data.R
@@ -16,6 +16,8 @@ library(spatstat)
 library(plotKML)
 library(sampSurf)
 library(quickPlot)
+library(exactextractr)
+library(Rcpp)
 
 # load spatial data
 
@@ -48,85 +50,111 @@ Plot(landcover)
 Plot(surface_water_cover)
 Plot(summer_solar_radiation)
 
-# load gpx data
+# load gpx data and population data
 
-## transects to consider
-#all_transects <- c('RG' , 'GL' , 'WM', 'TR' , 'NP' , 'CL' , 'LG' , 'MB' , 'HM'  , 'CT' , 'CB' , 'SH' , 'DC' , 'VB' , 'BG' , 'CM' , 'BC' , 'WS' , 'UL' , 'RL' , 'ER' , 'ME' , 'OBJ' , 'TC')
+#all_transects <- c('RG' , 'GL' , 'WM', 'TR' , 'NP' , 'CL' , 'LG' , 'MB' , 'HM'  , 'CT' , 'CB' , 'SH' , 'DC' , 'VB' , 'BG' , 'CM' , 'BC' , 'WS' , 'UL' , 'RL' , 'ER' , 'ME' , 'OBJ' , 'TC') ## transects to consider
 all_transects<-c("UL")
 
-## function to extract and clean data
-get_gpx_tracks <- function(names = all_transects) {
+## function to extract and clean gpx data
+get_gpx_tracks <- function(transects = all_transects) {
   gpx_tracks<-data.frame("transect"=character(),"longitude"=numeric(),"latitude"=numeric())
-  filenames <- paste0("~/Documents/GitHub/flax.rust/data/landscape.transect.data/gpx/cleaned/",names, ".gpx") 
-  for (name in names) {
-    file_name<- paste0("~/Documents/GitHub/flax.rust/data/landscape.transect.data/gpx/cleaned/",name, "_cleaned.gpx") 
+  filenames <- paste0("~/Documents/GitHub/flax.rust/data/landscape.transect.data/gpx/cleaned/",transects, ".gpx") 
+  for (transect in transects) {
+    file_name<- paste0("~/Documents/GitHub/flax.rust/data/landscape.transect.data/gpx/cleaned/",transect, "_cleaned.gpx") 
     GPX_points <- readGPX(file_name)$tracks[[1]][[1]] %>% dplyr::select(lon, lat) %>% dplyr::rename(Latitude = lat, Longitude = lon)
-    new_gpx_tracks<-data.frame("transect"=name,"longitude"= GPX_points$Longitude,"latitude"=GPX_points$Latitude)
+    new_gpx_tracks<-data.frame("transect"=transect,"longitude"= GPX_points$Longitude,"latitude"=GPX_points$Latitude)
     gpx_tracks<-rbind(gpx_tracks,new_gpx_tracks)
   }
   gpx_tracks
 }
 
-gpx_tracks <- get_gpx_tracks()
+gpx_tracks <- get_gpx_tracks() ## retrive data
+flax_pops <- read.csv("~/Documents/GitHub/flax.rust/data/landscape.transect.data/landscape.transects.csv") ## load flax population data
 
-# visualize gpx data
+# extract landscape data and plot flax populations
 
-## create line from gpx tracks
-line<-st_sfc(st_linestring(cbind(gpx_tracks$longitude,gpx_tracks$latitude))) ## draw line through all gps points
-
-## sample a point every meter along line
-n.points<-floor(as.numeric(st_length(st_sfc(line, crs = "+proj=longlat +datum=WGS84")))) ## set number of points to sample as distance of gps track in meters
-points<-st_line_sample(line,n=n.points,type="regular") ## 3 sample points at a reular interval along track
-
-## convert line and points to correct coordinate system
-line<-st_sfc(line, crs = "+proj=longlat +datum=WGS84") ## convert to coordinates
-points<-st_sfc(points, crs = "+proj=longlat +datum=WGS84")  ## convert to coordinates
-
-## create new line from sampled points
-new_line<-st_linestring(st_coordinates(points))
-new_line<-st_sfc(line, crs = "+proj=longlat +datum=WGS84")
-
-## visualize 
-
-### plot original track (blue)
-leaflet() %>% 
+## initiate map
+map<-leaflet() %>% 
   addTiles() %>% 
-  addPolylines(data=line,col="blue")
+  addMeasure(primaryLengthUnit = "meters")
 
-### plot original track (not visible), sampled points (green, appears as line due to high density), and new line (red)
-leaflet() %>% 
-  addTiles() %>% 
-  addPolylines(data=line,col="blue",weight=15) %>%
-  addCircles(data=st_cast(points,"POINT"),col="green",radius=5) %>%
-  addPolylines(data=new_line,col="red",weight=4)
+## plotting option
+plot<-T
 
-## create ribbon with width = 5m along 
+## loop to extract data
 
-line_mod <- st_transform(new_line, crs = 3501)
-plot(st_geometry(line_mod))
-ribbon <- st_buffer(line_mod, dist = 2.5, endCapStyle = "FLAT",joinStyle = "ROUND")
-plot(st_geometry(ribbon), add = TRUE,col="green")
+for(transect in all_transects) ### for each transect
+{
+  sub_gpx_tracks<-gpx_tracks[which(gpx_tracks$transect==transect),] #### subset gpx data
+  flax_sub_pop<-flax_pops[which(flax_pops$transect=="UL"),] #### subset flax population data
+  
+  line<-st_linestring(cbind(sub_gpx_tracks$longitude,sub_gpx_tracks$latitude)) #### draw line through all gps points
+  n.points<-floor(4*as.numeric(st_length(st_sfc(line, crs = "+proj=longlat +datum=WGS84")))) ### set number of points to sample such that sampling will occur every 0.25m (4* distance of gps track in meters)
+  points<-st_line_sample(line,n=n.points,type="regular") #### sample points at regular interval along track
+  points<-st_sfc(points,crs = "+proj=longlat +datum=WGS84") #### add crs to pio ts
+  
+  new_line<-st_linestring(st_coordinates(points)[,c("X","Y")]) #### convert points to line
+  new_line<-st_sfc(new_line, crs = "+proj=longlat +datum=WGS84") #### add crs
+  
+  if(plot) {map<-addPolylines(map=map,data = st_transform(new_line,crs="+proj=longlat +datum=WGS84"),col="black",weight=1)} #### add track to map
+  
+  track_points<-st_coordinates(new_line)[,c("X","Y")] #### extract raw track coordinates
+  transect.coded<-data.frame(track_points,"class"="nfz","chunk"=NA) #### set up data frame for flax populations, default class to 'no flax zone' = 'nfz
+  
+  #### subdivide gps data into 'chunks' of no flax/healthy flax/diseased flax
+  
+  chunk<-1 #### set starting chunk index to 1
+  
+  for(i in 1:nrow(flax_sub_pop)) ##### for each flax population
+  {
+    start.cords<-as.numeric(flax_sub_pop[i,c("start.long","start.lat")]) ##### get starting coordinates
+    start.point<-st_sfc(st_point(start.cords),crs = "+proj=longlat +datum=WGS84") ##### set crs
+    start.dists<-c(st_distance(st_cast(points,"POINT"),start.point)) ##### get distances between gps coordinates along transect line and population start point
+    start.index<-which.min(start.dists) ##### get index of minimun distance
+    
+    end.cords<-as.numeric(flax_sub_pop[i,c("end.long","end.lat")]) ##### get end coordinates
+    end.point<-st_sfc(st_point(end.cords),crs = "+proj=longlat +datum=WGS84") ##### set crs
+    end.dists<-c(st_distance(st_cast(points,"POINT"),end.point)) ##### get distances between gpx coordinates along transect line and population end point
+    end.index<-which.min(end.dists) ##### get index of minimum distance
+    
+    if(flax_sub_pop[i,"incidence"]==0) {new.status<-"fz"} ##### set class to 'flax zone' if there is no disease
+    if(flax_sub_pop[i,"incidence"]==1) {new.status<-"dfz"} ##### set class to 'diseased flax zone' if there is disease
+    
+    transect.coded[start.index:(end.index-40),"class"]<-new.status ##### set class of chunk containing flax pop, backing up 10m from recorded end pont (per sampling protocol)
+    transect.coded[which(is.na(transect.coded[1:start.index,"chunk"])),"chunk"]<-chunk ##### set chunk index of chunk with class nfz preceeding flax population
+    transect.coded[start.index:(end.index-40),"chunk"]<-chunk+1 ##### set chunk index of flax population , backing up 10m from recorded end pont (per sampling protocol)
+    chunk<-chunk+ 2 ##### set next hunk index
+    if(i==nrow(flax_sub_pop)) {transect.coded[which(is.na(transect.coded$chunk)),"chunk"]<-chunk} ##### after the last flax population, set chunk of remaining nfz if it exists
+  }
+  
+  #### for each chunk, extract landscape data and plot 
+  for(chunk in 1:max(transect.coded$chunk))
+  {
+    sub.transect.coded<-transect.coded[which(transect.coded$chunk==chunk),] ##### subset data
+  
+    if(sub.transect.coded[1,"class"]=="nfz") {col<-"grey"} ##### set plot color based on class
+    if(sub.transect.coded[1,"class"]=="fz") {col<-"green"}
+    if(sub.transect.coded[1,"class"]=="dfz") {col<-"yellow"}
+    
+    
+    line<-st_linestring(cbind(sub.transect.coded$X,sub.transect.coded$Y)) ##### draw line through all gps points
+    line<-st_sfc(line, crs = "+proj=longlat +datum=WGS84") ##### convert to coordinates
+    line_mod <- st_transform(line, crs = 3501) ##### set crs
+    ribbon <- st_buffer(line_mod, dist = 2.5, endCapStyle = "FLAT",joinStyle = "ROUND") ##### draw polygon around line
+  
+    if(plot) {map<-addPolygons(map=map,data = st_transform(ribbon,crs="+proj=longlat +datum=WGS84"),col=col)} ##### add polygon to map
+    
+    sample_points<-st_sample(ribbon,size=as.numeric(floor(st_area(ribbon))),type="regular") ##### regularly sample 1 point per m2 from polygon 
+    
+    #if(plot) {map<-addCircleMarkers(map=map,data=st_transform(sample_points,crs="+proj=longlat +datum=WGS84"),col="black",radius = .1,weight=0)}
+    
+    unlist(raster::extract(x=topography,y=as(sample_points,"Spatial"),method="bilinear"))->x
+    length(x)
+    dim(st_coordinates(sample_points))
+    }
+}
 
-leaflet() %>% 
-  addTiles() %>% 
-  addMeasure(primaryLengthUnit = "meters") %>% 
-  addPolylines(data = st_transform(line_mod,crs="+proj=longlat +datum=WGS84")) %>% 
-  addPolygons(data = st_transform(ribbon,crs="+proj=longlat +datum=WGS84"),col="red")
 
-
-# load flax population data
-
-flax_pops <- read.csv("~/Documents/GitHub/flax.rust/data/landscape.transect.data/landscape.transects.csv")
-
-test.point<-st_sfc(st_point(cbind(gpx_tracks$longitude[1],gpx_tracks$latitude[1])),crs = "+proj=longlat +datum=WGS84")
-dists<-c(st_distance(st_cast(points,"POINT"),test.point))
-
-x<-st_distance(test.point,st_cast(points,"POINT"))
-
-
-library(sf)
-
-
-
+raster::extract(x=topography,y=as(ribbon,"Spatial"),method="bilinear")->x
 
 
