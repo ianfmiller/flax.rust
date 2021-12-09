5bb8ae8c6e8f37ca3bb929303a7b2f157a2290d3
ifmiller <ifmiller@princeton.edu>
2021-12-09 18:37:49 -0500

    update starting plant infection intensity script with new data/file names, calculate mean starting infection intensity

--- cross.scale.transmission.dynamics/starting plant inf intens model.R
+++ cross.scale.transmission.dynamics/starting plant inf intens model.R
@@ -1,25 +1,25 @@
 plot<-F
 
-source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant data prep.R")
+source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/infection intensity data prep.R")
 source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant loc dataset building.R")
 
 epi.obs.dates<-list("CC"=c("2020-06-22","2020-06-29","2020-07-06","2020-07-13","2020-07-20","2020-07-27"),"BT"=c("2020-06-24","2020-07-01","2020-07-08"),"GM"=c("2020-06-23","2020-06-30","2020-07-02","2020-07-07","2020-07-09","2020-07-15"),"HM"=c("2020-06-25","2020-07-02","2020-07-07","2020-07-09","2020-07-15"))    
 
 data.vec<-c() 
-for(tag in unique(plants$Tag)) # loop to pull out plant inf intens of newly diseased plants
+for(tag in unique(infection.intensity$Tag)) # loop to pull out plant inf intens of newly diseased plants
 {
   if((tag %in% corrected.epi$Tag))
   {
     inf.date<-corrected.epi[which(corrected.epi$Tag==tag),"Date.First.Observed.Diseased"]
-    sub.plant.data<-plants[which(plants$Tag==tag),]
-    site<-sub.plant.data[1,"Site"]
+    sub.data<-infection.intensity[which(infection.intensity$Tag==tag),]
+    site<-sub.data[1,"Site"]
     if(site=="BT") {min.date<-"2020-06-24"}
     if(site=="CC") {min.date<-"2020-06-24"}
     if(site=="GM") {min.date<-"2020-06-23"}
     if(site=="HM") {min.date<-"2020-06-25"}
     if(inf.date>=min.date) {
-      index<-which(sub.plant.data$Date==inf.date)
-      data.vec<-c(data.vec,sub.plant.data[index,"plant.inf.intens"])
+      index<-which(sub.data$Date==inf.date)
+      data.vec<-c(data.vec,sub.data[index,"infection.intensity"])
     }
   }
 }
@@ -30,6 +30,8 @@ y<-model.data$density
 x<-model.data$mids
 mod<-nls(y~c*exp(-c*x),start=list(c=.5))
 lambda<-summary(mod)$coefficients[1]
+
+mean.starting.plant.inf.intens<-mean(data.vec)
 
 starting.plant.inf.intens.mod<-function(x) {log(1-x)/-2.103}
 
