d2966844dfb92ec471d6b78d330c50c30775a79c
ifmiller <ifmiller@princeton.edu>
2021-12-09 17:14:34 -0500

    update height change prediction funcs

--- cross.scale.transmission.dynamics/predict plant height change funcs.R
+++ cross.scale.transmission.dynamics/predict plant height change funcs.R
@@ -1,5 +1,4 @@
 # load data and model
-library(MASS)
 source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/prep.enviro.data.R")
 plant.growth.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/models/plant.growth.model.RDS")
 
@@ -31,21 +30,15 @@ predict.plant.growth<-function(height.last,site,date0,date1,exclude.site=T)
   new.max.temp<-max(temp.rh.sub$temp.c,na.rm = T) #max temperature
   new.min.temp<-min(temp.rh.sub$temp.c,na.rm = T) #min temperature
   
-  abs.hum<-6.112*exp((17.67*temp.rh.sub$temp.c)/(temp.rh.sub$temp.c+243.5))*temp.rh.sub$rh*2.1674/(273.15+T)
-  new.mean.abs.hum<-mean(abs.hum,na.rm=T) #absolute humidity, see https://www.medrxiv.org/content/10.1101/2020.02.12.20022467v1.full.pdf
+  abs.hum<-0.1324732*exp((17.67*temp.rh.sub$temp.c)/(temp.rh.sub$temp.c+243.5))*temp.rh.sub$rh/(273.15+T)
+  new.mean.abs.hum<-mean(abs.hum,na.rm=T)
   new.max.abs.hum<-max(abs.hum,na.rm=T)
   new.min.abs.hum<-min(abs.hum,na.rm=T)
   
-  #svps<- 0.6108 * exp(17.27 * temp.rh.sub$temp.c / (temp.rh.sub$temp.c + 237.3)) #saturation vapor pressures
-  #avps<- temp.rh.sub$rh / 100 * svps #actual vapor pressures 
-  #vpds<-avps-svps
-  
-  #new.mean.vpd<-mean(vpds,na.rm=T)
-  #new.max.vpd<-max(vpds,na.rm=T)
-  #new.min.vpd<-min(vpds,na.rm=T)
-  
-  new.tot.rain<-sum(weath.sub$rain,na.rm=T)
+  new.mean.wetness<-mean(weath.sub$wetness,na.rm = T)
+  new.mean.daily.rain<-sum(weath.sub$rain,na.rm=T)
   new.mean.solar<-mean(weath.sub$solar.radiation,na.rm=T)
+  new.mean.soil.moisture<-mean(weath.sub$soil.moisture,na.rm=T)
 
   delta.days<-as.numeric(date1-date0)
   
@@ -53,7 +46,7 @@ predict.plant.growth<-function(height.last,site,date0,date1,exclude.site=T)
   pred.data<-data.frame("time"=delta.days,"height"=height.last,
                         "mean.temp"=new.mean.temp,"max.temp"=new.max.temp,"min.temp"=new.min.temp,
                         "mean.abs.hum"=new.mean.abs.hum,"max.abs.hum"=new.max.abs.hum,"min.abs.hum"=new.min.abs.hum,
-                        "mean.solar"=new.mean.solar,"tot.rain"=new.tot.rain,
+                        "mean.wetness"=new.mean.wetness,"mean.daily.rain"=new.mean.daily.rain,"mean.solar"=new.mean.solar,"mean.soil.moisture"=new.mean.soil.moisture,
                         "site"=site)
   
   if(exclude.site) {height.next<-height.last+predict(plant.growth.model,newdata=pred.data,exclude = 's(site)')} else {height.next<-height.last+predict(plant.growth.model,newdata=pred.data)}
@@ -85,21 +78,15 @@ predict.plant.growth.boot<-function(height.last,site,date0,date1)
   new.max.temp<-max(temp.rh.sub$temp.c,na.rm = T) #max temperature
   new.min.temp<-min(temp.rh.sub$temp.c,na.rm = T) #min temperature
   
-  abs.hum<-6.112*exp((17.67*temp.rh.sub$temp.c)/(temp.rh.sub$temp.c+243.5))*temp.rh.sub$rh*2.1674/(273.15+T)
-  new.mean.abs.hum<-mean(abs.hum,na.rm=T) #absolute humidity, see https://www.medrxiv.org/content/10.1101/2020.02.12.20022467v1.full.pdf
+  abs.hum<-0.1324732*exp((17.67*temp.rh.sub$temp.c)/(temp.rh.sub$temp.c+243.5))*temp.rh.sub$rh/(273.15+T)
+  new.mean.abs.hum<-mean(abs.hum,na.rm=T) 
   new.max.abs.hum<-max(abs.hum,na.rm=T)
   new.min.abs.hum<-min(abs.hum,na.rm=T)
-  
-  #svps<- 0.6108 * exp(17.27 * temp.rh.sub$temp.c / (temp.rh.sub$temp.c + 237.3)) #saturation vapor pressures
-  #avps<- temp.rh.sub$rh / 100 * svps #actual vapor pressures 
-  #vpds<-avps-svps
-  
-  #new.mean.vpd<-mean(vpds,na.rm=T)
-  #new.max.vpd<-max(vpds,na.rm=T)
-  #new.min.vpd<-min(vpds,na.rm=T)
-  
-  new.tot.rain<-sum(weath.sub$rain,na.rm=T)
+
+  new.mean.wetness<-mean(weath.sub$wetness,na.rm = T)
+  new.mean.daily.rain<-sum(weath.sub$rain,na.rm=T)
   new.mean.solar<-mean(weath.sub$solar.radiation,na.rm=T)
+  new.mean.soil.moisture<-mean(weath.sub$soil.moisture,na.rm=T)
   
   delta.days<-as.numeric(date1-date0)
   
@@ -107,7 +94,7 @@ predict.plant.growth.boot<-function(height.last,site,date0,date1)
   pred.data<-data.frame("time"=delta.days,"height"=height.last,
                         "mean.temp"=new.mean.temp,"max.temp"=new.max.temp,"min.temp"=new.min.temp,
                         "mean.abs.hum"=new.mean.abs.hum,"max.abs.hum"=new.max.abs.hum,"min.abs.hum"=new.min.abs.hum,
-                        "mean.solar"=new.mean.solar,"tot.rain"=new.tot.rain,
+                        "mean.solar"=new.mean.solar,"mean.daily.rain"=new.mean.daily.rain,
                         "site"=site)
   Xp <- predict(plant.growth.model, newdata = pred.data, exlude='s(site)',type="lpmatrix")
   beta <- coef(plant.growth.model) ## posterior mean of coefs
@@ -149,21 +136,15 @@ predict.plant.growth.last<-function(height.next,site,date0,date1,exclude.site=T)
   new.max.temp<-max(temp.rh.sub$temp.c,na.rm = T) #max temperature
   new.min.temp<-min(temp.rh.sub$temp.c,na.rm = T) #min temperature
   
-  abs.hum<-6.112*exp((17.67*temp.rh.sub$temp.c)/(temp.rh.sub$temp.c+243.5))*temp.rh.sub$rh*2.1674/(273.15+T)
-  new.mean.abs.hum<-mean(abs.hum,na.rm=T) #absolute humidity, see https://www.medrxiv.org/content/10.1101/2020.02.12.20022467v1.full.pdf
+  abs.hum<-0.1324732*exp((17.67*temp.rh.sub$temp.c)/(temp.rh.sub$temp.c+243.5))*temp.rh.sub$rh/(273.15+T)
+  new.mean.abs.hum<-mean(abs.hum,na.rm=T)
   new.max.abs.hum<-max(abs.hum,na.rm=T)
   new.min.abs.hum<-min(abs.hum,na.rm=T)
   
-  #svps<- 0.6108 * exp(17.27 * temp.rh.sub$temp.c / (temp.rh.sub$temp.c + 237.3)) #saturation vapor pressures
-  #avps<- temp.rh.sub$rh / 100 * svps #actual vapor pressures 
-  #vpds<-avps-svps
-  
-  #new.mean.vpd<-mean(vpds,na.rm=T)
-  #new.max.vpd<-max(vpds,na.rm=T)
-  #new.min.vpd<-min(vpds,na.rm=T)
-  
-  new.tot.rain<-sum(weath.sub$rain,na.rm=T)
+  new.mean.wetness<-mean(weath.sub$wetness,na.rm = T)
+  new.mean.daily.rain<-sum(weath.sub$rain,na.rm=T)
   new.mean.solar<-mean(weath.sub$solar.radiation,na.rm=T)
+  new.mean.soil.moisture<-mean(weath.sub$soil.moisture,na.rm=T)
   
   delta.days<-as.numeric(date1-date0)
   
@@ -174,7 +155,7 @@ predict.plant.growth.last<-function(height.next,site,date0,date1,exclude.site=T)
     pred.data<-data.frame("time"=delta.days,"height"=plant.height.last.test,
                            "mean.temp"=new.mean.temp,"max.temp"=new.max.temp,"min.temp"=new.min.temp,
                            "mean.abs.hum"=new.mean.abs.hum,"max.abs.hum"=new.max.abs.hum,"min.abs.hum"=new.min.abs.hum,
-                           "mean.solar"=new.mean.solar,"tot.rain"=new.tot.rain,
+                           "mean.solar"=new.mean.solar,"mean.daily.rain"=new.mean.daily.rain,
                            "site"=site)
     if(exclude.site) {plant.height.next.pred<-plant.height.last.test+predict(plant.growth.model,newdata=pred.data,exclude = 's(site)')} else {plant.height.next.pred<-plant.height.last.test+predict(plant.growth.model,newdata=pred.data)}
     abs(plant.height.next.pred-height.next)
