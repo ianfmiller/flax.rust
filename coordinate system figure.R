c887f816e0542c900ec3c9b0b6e91cfa32237a39
ifmiller <ifmiller@princeton.edu>
2021-12-20 12:43:59 -0500

    update fig, make panels for positive and negative X

--- coordinate system figure.R
+++ coordinate system figure.R
@@ -1,43 +1,64 @@
-par(mfrow=c(2,2),mar=c(2,2,2,2))
-plot(0,0,xlim=c(-2.5,2.5),ylim=c(-2.5,2),type="n",asp=1,xlab="",ylab="")
-xorigin<-.25
-yorigin<-.25
-degree<-45
+par(mfrow=c(1,2),mar=c(2,2,2,2))
+
+plot(0,0,xlim=c(-2.5,2),ylim=c(-2.5,2),type="n",asp=1,xlab="",ylab="",main="X < 0",cex.main=2)
+mtext("A",adj=1,font=2)
+xorigin<-0
+yorigin<-0
+degree<-2*pi*(45/360)
 xs<-xorigin
 xw<-xs+sin(degree)
 ys<-yorigin
 yw<-ys+cos(degree)
-xt<--1
-yt<--1
+xt<--1.5
+yt<--3
 c<-((xt-xs)*(xw-xs)+(yt-ys)*(yw-ys))/((xw-xs)^2+(yw-ys)^2)
 xp<-xs+c*(xw-xs)
 yp<-ys+c*(yw-ys)
 
-arrows(xs,ys,xs+1.5*(xw-xs),ys+1.5*(yw-ys),lwd=4)
-segments(xs,ys,xp,yp,lty=2,col="blue",lwd=4)
+arrows(xs,ys,xs+2*(xw-xs),ys+2*(yw-ys),lwd=4,col="grey")
+segments(xs,ys,xp,yp,lty=2,col="red",lwd=4)
 segments(xp,yp,xt,yt,lty=2,col="blue",lwd=4)
-segments(xs,ys,xt,yt,lty=2,col="blue",lwd=4)
 
-points(xp,yp,cex=3,pch=16)
-text(xp,yp,expression((x[P]*','*y[P])),pos=4,cex=1.5)
+points(xp,yp,cex=2,pch=16,col="purple")
+text(xp,yp,expression((x[P]*','*y[P])),pos=4,cex=1.5,offset=1,col="purple")
 points(xs,ys,cex=3,pch=16)
 text(xs,ys,expression((x[S]*','*y[S])),pos=2,cex=1.5)
-points(xw,yw,cex=3,pch=16)
-text(xw,yw,expression((x[W]*','*y[W])),pos=4,cex=1.5)
-points(xt,yt,cex=3,pch=16)
-text(xt,yt,expression((x[T]*','*y[T])),pos=4,cex=1.5)
-text(xs+1.5*(xw-xs),ys+1.5*(yw-ys),"W",pos=3,cex=1.5)
+points(xw,yw,cex=2,pch=16,col="grey")
+text(xw,yw,expression((x[W]*','*y[W])),pos=4,cex=1.5,offset=1,col="grey")
+points(xt,yt,cex=3,pch=16,col="orange")
+text(xt,yt,expression((x[T]*','*y[T])),pos=4,cex=1.5,col="orange")
+text(xs+2*(xw-xs),ys+2*(yw-ys),"W",pos=3,cex=1.5,offset=1,col="grey")
+text(-1.5,-1,"X",cex=1.5,col="red")
+text(-2.1,-2.9,"Y",cex=1.5,col="blue")
 
-s=-1*((yw-ys)/(xw-xs))^-1
-curve(ys+s*(x-xs),add=T)
+plot(0,0,xlim=c(-1,3.5),ylim=c(-1,3.5),type="n",asp=1,xlab="",ylab="",main="X > 0",cex.main=2)
+mtext("B",adj=1,font=2)
+xorigin<-0
+yorigin<-0
+degree<-2*pi*(45/360)
+xs<-xorigin
+xw<-xs+sin(degree)
+ys<-yorigin
+yw<-ys+cos(degree)
+xt<-1
+yt<-4
+c<-((xt-xs)*(xw-xs)+(yt-ys)*(yw-ys))/((xw-xs)^2+(yw-ys)^2)
+xp<-xs+c*(xw-xs)
+yp<-ys+c*(yw-ys)
 
-points(xp,yp)
-text(xp,yp,expression((x[P]*','*y[P])),pos=4)
-segments(xs,ys,xp,yp,lty=2,col="blue")
-segments(xp,yp,xtarget,ytarget,lty=2,col="blue")
-segments(xs,ys,xtarget,ytarget,lty=2,col="blue")
+arrows(xs,ys,xs+2*(xw-xs),ys+2*(yw-ys),lwd=4,col="grey")
+segments(xs,ys,xp,yp,lty=2,col="red",lwd=4)
+segments(xp,yp,xt,yt,lty=2,col="blue",lwd=4)
 
-s=-1*((yw-ys)/(xw-xs))^-1 #slope of perpendicular line
+points(xp,yp,cex=2,pch=16,col="purple")
+text(xp,yp,expression((x[P]*','*y[P])),pos=4,cex=1.5,offset=1,col="purple")
+points(xs,ys,cex=3,pch=16)
+text(xs,ys,expression((x[S]*','*y[S])),pos=2,cex=1.5)
+points(xw,yw,cex=2,pch=16,col="grey")
+text(xw,yw,expression((x[W]*','*y[W])),pos=4,cex=1.5,offset=1,col="grey")
+points(xt,yt,cex=3,pch=16,col="orange")
+text(xt,yt,expression((x[T]*','*y[T])),pos=4,cex=1.5,col="orange")
+text(xs+2*(xw-xs),ys+2*(yw-ys),"W",pos=3,cex=1.5,offset=1,col="grey")
+text(2.3,2,"X",cex=1.5,col="red")
+text(2.1,3.25,"Y",cex=1.5,col="blue")
 
-curve(((yw-ys)/(xw-xs))*x-((yw-ys)/(xw-xs))*xs+ys,add=T,lty=2,col="red")
-curve(s*x+ys+s*xs,add=T,col="red",lty=2)
