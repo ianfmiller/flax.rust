7cb2d942323785c242a771fdf09f51b90b52f6c8
ifmiller <ifmiller@princeton.edu>
2022-01-04 13:48:16 -0500

    update foi model visualization

--- cross.scale.transmission.dynamics/foi analysis.R
+++ cross.scale.transmission.dynamics/foi analysis.R
@@ -75,7 +75,7 @@ foi.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynami
 layout(matrix(c(1,1,1,1,2,3,3,3,3,4,4,4,4,5,5,5,5,18,14,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,15,16,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,17),3,18,byrow = T))
 
 par(mar=c(4,7,3,1.5))
-plot(foi.model,select = 1,scheme = 2,zlim=c(-8,8),too.far=.5,hcolors=rev(heat.colors(101)),contour.col="black",labcex=.75,cex.lab=1.5,cex.axis=1.5,xlab=expression(log[10]*' predicted spore deposition'),ylab="plant height (cm)",main="",rug=F)
+plot(foi.model,select = 1,scheme = 2,zlim=c(-8,8),too.far=.5,hcolors=rev(heat.colors(101)),contour.col="black",cex.lab=1.5,cex.axis=1.5,xlab=expression(log[10]*' predicted spore deposition'),ylab="plant height (cm)",main="",rug=F)
 points(log10(foi.data$foi),foi.data$height.cm,pch=".")
 par(mar=c(4,1,3,4))
 plot(0,0,type="n",xlim=c(0,1),ylim=c(-8-0.08,8+0.08),axes=F,xlab="",ylab="")
@@ -114,60 +114,46 @@ mtext("H",adj=1,cex=1.25,font=2)
 plot(foi.model,select = 9,shade=T,main="",cex.lab=1.5,cex.axis=1.5,xlab="total rainfall (mm)",ylab="s(total rainfall)")
 grid()
 mtext("I",adj=1,cex=1.25,font=2)
-plot(plant.growth.model,select = 10,shade=T,main="",cex.lab=1.5,cex.axis=1.5,xlab="mean leaf wetness (%)",ylab="s(mean leaf wetness)")
+plot(foi.model,select = 10,shade=T,main="",cex.lab=1.5,cex.axis=1.5,xlab="mean leaf wetness (%)",ylab="s(mean leaf wetness)")
 grid()
 mtext("J",adj=1,cex=1.25,font=2)
 plot(foi.model,select = 11,shade=T,main="",cex.lab=1.5,cex.axis=1.5,ylab="s(tag)")
 grid()
-mtext("L",adj=1,cex=1.25,font=2)
+mtext("K",adj=1,cex=1.25,font=2)
 plot(foi.model,select = 12,shade=T,main="",cex.lab=1.5,cex.axis=1.5,ylab="s(site)")
 grid()
-mtext("M",adj=1,cex=1.25,font=2)
-
+mtext("L",adj=1,cex=1.25,font=2)
 
 par(mar=c(6,8,6,0))
 layout(matrix(c(1,1,1,1,2),1,5))
-plot(log10(foi.data$foi),jitter(foi.data$status.next*.1,factor=.5),xlab=expression('predicted '*log[10]*' spore depositin'),ylab="",axes=F,cex.lab=2,ylim=c(-.025,0.125),xlim=c(-6,3))
-axis(2,at=c(0,.1),labels = c("healthy","infected"),cex.axis=2,line=3,tick = F)
-axis(2,at=c(0,.05,.1),cex.axis=2,col="red",col.axis="red")
+plot(log10(foi.data$foi),jitter(foi.data$status.next*1,factor=.5),xlab=expression('predicted '*log[10]*' spore depositin'),ylab="",axes=F,cex.lab=2,ylim=c(-0.1,1.1),xlim=c(-6,3))
+axis(2,at=c(0,1),labels = c("healthy","infected"),cex.axis=2,line=3,tick = F)
+axis(2,at=c(0,.5,1),cex.axis=2,col="red",col.axis="red")
 mtext("odds of infection",side=2,line=2.5,cex=2,col="red")
 axis(1,cex.axis=1.5)
 
 fois<-10^seq(-6,2.3,.1)
-new.data.1<-data.frame("foi"=fois,
-                     "height.cm"=5,
-                     "site"="CC"
-                     )
-new.data.2<-data.frame("foi"=fois,
-                       "height.cm"=10,
-                       "site"="CC"
-                      )
-new.data.3<-data.frame("foi"=fois,
-                       "height.cm"=25,
-                       "site"="CC"
-                      )
-new.data.4<-data.frame("foi"=fois,
-                       "height.cm"=50,
-                       "site"="CC"
-                      )
-points(log10(new.data.1$foi),predict(foi.model,newdata = new.data.1,type = "response",exclude = "s(site)"),type="l",col="red",lwd=4,lty=1)
-points(log10(new.data.2$foi),predict(foi.model,newdata = new.data.2,type = "response",exclude = "s(site)"),type="l",col="red",lwd=4,lty=2)
-points(log10(new.data.3$foi),predict(foi.model,newdata = new.data.3,type = "response",exclude = "s(site)"),type="l",col="red",lwd=4,lty=3)
-points(log10(new.data.4$foi),predict(foi.model,newdata = new.data.4,type = "response",exclude = "s(site)"),type="l",col="red",lwd=4,lty=4)
+
+new.data.1<-foi.data[1:length(fois),]
+new.data.1$foi<-fois
+new.data.1$height.cm<-5
+
+new.data.2<-foi.data[1:length(fois),]
+new.data.2$foi<-fois
+new.data.2$height.cm<-10
+
+new.data.3<-foi.data[1:length(fois),]
+new.data.3$foi<-fois
+new.data.3$height.cm<-25
+
+new.data.4<-foi.data[1:length(fois),]
+new.data.4$foi<-fois
+new.data.4$height.cm<-50
+
+points(log10(new.data.1$foi),predict(foi.model,newdata = new.data.1,type = "response",exclude = c("s(site)","s(tag)","s(mean.temp)","s(max.temp)","s(min.temp)","s(mean.abs.hum)","s(max.abs.hum)","s(min.abs.hum)","s(mean.solar)","s(mean.daily.rain)","s(mean.wetness)")),type="l",col="red",lwd=4,lty=1)
+points(log10(new.data.2$foi),predict(foi.model,newdata = new.data.2,type = "response",exclude = c("s(site)","s(tag)","s(mean.temp)","s(max.temp)","s(min.temp)","s(mean.abs.hum)","s(max.abs.hum)","s(min.abs.hum)","s(mean.solar)","s(mean.daily.rain)","s(mean.wetness)")),type="l",col="red",lwd=4,lty=2)
+points(log10(new.data.3$foi),predict(foi.model,newdata = new.data.3,type = "response",exclude = c("s(site)","s(tag)","s(mean.temp)","s(max.temp)","s(min.temp)","s(mean.abs.hum)","s(max.abs.hum)","s(min.abs.hum)","s(mean.solar)","s(mean.daily.rain)","s(mean.wetness)")),type="l",col="red",lwd=4,lty=3)
+points(log10(new.data.4$foi),predict(foi.model,newdata = new.data.4,type = "response",exclude = c("s(site)","s(tag)","s(mean.temp)","s(max.temp)","s(min.temp)","s(mean.abs.hum)","s(max.abs.hum)","s(min.abs.hum)","s(mean.solar)","s(mean.daily.rain)","s(mean.wetness)")),type="l",col="red",lwd=4,lty=4)
 par(mar=c(0,0,0,0))
 plot(0,0,type="n",axes=F,xlab = "",ylab="")
 legend("left",legend=c("50cm","25cm","10cm","5cm"),col="red",lty=c(4,3,2,1),bty="n",cex=2,lwd=4)
-
-par(mfrow=c(1,1),mar=c(6,8,6,6))
-plot(0,0,type = "n",xlab=expression('predicted '*log[10]*' spore depositin'),ylab="relative odds of infection",axes=F,cex.lab=2,ylim=c(0,100),xlim = c(-6,3))
-axis(1)
-axis(2)
-base.pred<-predict(foi.model,newdata = new.data.1,type = "response",re.form=NA)[1]
-points(log10(new.data.1$foi),predict(foi.model,newdata = new.data.1,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=4,lty=1)
-points(log10(new.data.2$foi),predict(foi.model,newdata = new.data.2,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=4,lty=2)
-points(log10(new.data.3$foi),predict(foi.model,newdata = new.data.3,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=4,lty=3)
-points(log10(new.data.4$foi),predict(foi.model,newdata = new.data.4,type = "response",re.form=NA)/base.pred,type="l",col="red",lwd=4,lty=4)
-legend("topleft",legend=c("50cm","25cm","10cm","5cm"),col="red",lty=c(4,3,2,1),bty="n",cex=2,lwd=4)
-
-
-         
