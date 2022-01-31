1accf7d639ba6cc49272f766aa2cb134577d8964
ifmiller <ifmiller@princeton.edu>
2022-01-31 17:55:07 -0500

    set 0.1 as floor of infection intensity rather than (0) during prediction, as log10 transformation is now applied

--- cross.scale.transmission.dynamics/predict plant inf intens change funcs.R
+++ cross.scale.transmission.dynamics/predict plant inf intens change funcs.R
@@ -127,7 +127,7 @@ predict.inf.intens.boot.alt<-function(inf.intens.last,max.height.last,mean.temp.
     preds[j]   <- ilink(Xp %*% mrand[j, ])
   }
   inf.intens.next<-inf.intens.last+delta.days*preds[1]
-  if(inf.intens.next<.1) {inf.intens.next<-0}
+  if(inf.intens.next<.1) {inf.intens.next<-.1}
   inf.intens.next
 }
 
