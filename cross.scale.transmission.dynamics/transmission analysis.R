b71804d1e6d7239ad68c67ab47a65478a13529f4
ifmiller <ifmiller@princeton.edu>
2022-02-01 15:41:20 -0500

    update transmission analysis, refit model

--- cross.scale.transmission.dynamics/transmission analysis.R
+++ cross.scale.transmission.dynamics/transmission analysis.R
@@ -11,6 +11,7 @@ for(i in 1:nrow(transmission.data))
   if(is.na(transmission.data[i,"tag"])) {transmission.data[i,"tag"]<-paste0(transmission.data[i,"site"],"X",transmission.data[i,"X"]+transmission.data[i,"x"],"Y",transmission.data[i,"Y"]+transmission.data[i,"y"])}
 }
 transmission.data$tag<-as.factor(transmission.data$tag)
+transmission.data$log.10.spore.deposition.per.day<-log10(transmission.data$tot.spore.deposition)/transmission.data$time
 
 # analyze data
 
@@ -20,15 +21,16 @@ if(!file.exists("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/
 {
   set.seed(9843525)
   mod<-bam(status.next~
-             te(log10(tot.spore.deposition),height.cm,sp=c(1e10,-1,-1))+ #When fitting the tensor, we fixed the smoothing parameter for the marginal smooth for log10 total spore deposition to 1e10. This effectively sets the shape of this marginal smooth to be linear, preventing any overfitting involving non-monotonic effects of spore deposition.
+             te(log.10.spore.deposition.per.day,height.cm,sp=c(1e10,-1,-1))+ #When fitting the tensor, we fixed the smoothing parameter for the marginal smooth for log10 total spore deposition to 1e10. This effectively sets the shape of this marginal smooth to be linear, preventing any overfitting involving non-monotonic effects of spore deposition.
              s(mean.temp)+
              s(max.temp)+
              s(min.temp)+
              s(mean.abs.hum)+
              s(mean.daily.rain)+
              s(tag,bs="re")+
-             s(site,bs="re"),
-           family=binomial(link="logit"),
+             s(site,bs="re")+
+             offset(log(time)),
+           family=binomial(link="cloglog"),
            select = T,
            method="fREML",
            data=transmission.data,
@@ -44,22 +46,5 @@ transmission.model<-readRDS("~/Documents/GitHub/flax.rust/cross.scale.transmissi
 ## model checking
 par(mfrow=c(2,2))
 set.seed(9843525)
-dummy.transmission.data<-transmission.data
-dummy.transmission.data$log.10.tot.spore.deposition<-log10(transmission.data$tot.spore.deposition)
-dummy.mod<-bam(status.next~
-           te(log.10.tot.spore.deposition,height.cm,sp=c(1e10,-1,-1))+ #Fit dummy mod with hard coded log.10.tot.spore.deposition as predictor because the log10 function throws off gam.check
-           s(mean.temp)+
-           s(max.temp)+
-           s(min.temp)+
-           s(mean.abs.hum)+
-           s(mean.daily.rain)+
-           s(tag,bs="re")+
-           s(site,bs="re"),
-         family=binomial(link="logit"),
-         select = T,
-         method="fREML",
-         data=transmission.data,
-         control = list(nthreads=4),
-         discrete = T)
-gam.check(dummy.mod) #No issues
+gam.check(transmission.model) #No issues
 concurvity(transmission.model,full=F) #Concurvity is expected between all weather variables. The non-pessimistic estimations of concurvity (under $estimate and $observed) indicate that it is not a serious issue in the model fit.
