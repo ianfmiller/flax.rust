ca474703cadd42d22911df71b09cf9e1ce423d08
ifmiller <ifmiller@princeton.edu>
2022-02-15 18:32:11 -0500

    polish fig s1

--- cross.scale.transmission.dynamics/figs/figure S1.R
+++ cross.scale.transmission.dynamics/figs/figure S1.R
@@ -1,7 +1,8 @@
 par(mfrow=c(1,2),mar=c(2,2,2,2))
 
-plot(0,0,xlim=c(-2.5,2),ylim=c(-2.5,2),type="n",asp=1,xlab="",ylab="",main="X < 0",cex.main=2)
-mtext("A",adj=1,font=2)
+plot(0,0,xlim=c(-2.5,2),ylim=c(-3.5,2),type="n",asp=1,xlab="",ylab="",main=expression(x[T]<x[S]),cex.main=2,axes=F)
+box()
+mtext("A",adj=1,cex=1.5)
 xorigin<-0
 yorigin<-0
 degree<-2*pi*(45/360)
@@ -16,6 +17,7 @@ xp<-xs+c*(xw-xs)
 yp<-ys+c*(yw-ys)
 
 arrows(xs,ys,xs+2*(xw-xs),ys+2*(yw-ys),lwd=4,col="grey")
+arrows(xs,ys,xs-0.75*(xw-xs),ys-0.75*(yw-ys),lwd=4,col="grey")
 segments(xs,ys,xp,yp,lty=2,col="red",lwd=4)
 segments(xp,yp,xt,yt,lty=2,col="blue",lwd=4)
 
@@ -28,11 +30,12 @@ text(xw,yw,expression((x[W]*','*y[W])),pos=4,cex=1.5,offset=1,col="grey")
 points(xt,yt,cex=3,pch=16,col="orange")
 text(xt,yt,expression((x[T]*','*y[T])),pos=4,cex=1.5,col="orange")
 text(xs+2*(xw-xs),ys+2*(yw-ys),"W",pos=3,cex=1.5,offset=1,col="grey")
-text(-1.5,-1,"X",cex=1.5,col="red")
-text(-2.1,-2.9,"Y",cex=1.5,col="blue")
+text(-1.5,-1,expression(bar("X")),cex=1.5,col="red")
+text(-2.1,-2.9,expression(bar("Y")),cex=1.5,col="blue")
 
-plot(0,0,xlim=c(-1,3.5),ylim=c(-1,3.5),type="n",asp=1,xlab="",ylab="",main="X > 0",cex.main=2)
-mtext("B",adj=1,font=2)
+plot(0,0,xlim=c(-1,3.5),ylim=c(0,4.5),type="n",asp=1,xlab="",ylab="",main=expression(x[T]>x[S]),cex.main=2,axes=F)
+box()
+mtext("B",adj=1,cex=1.5)
 xorigin<-0
 yorigin<-0
 degree<-2*pi*(45/360)
@@ -47,6 +50,7 @@ xp<-xs+c*(xw-xs)
 yp<-ys+c*(yw-ys)
 
 arrows(xs,ys,xs+2*(xw-xs),ys+2*(yw-ys),lwd=4,col="grey")
+arrows(xs,ys,xs-0.5*(xw-xs),ys-0.5*(yw-ys),lwd=4,col="grey")
 segments(xs,ys,xp,yp,lty=2,col="red",lwd=4)
 segments(xp,yp,xt,yt,lty=2,col="blue",lwd=4)
 
@@ -59,6 +63,7 @@ text(xw,yw,expression((x[W]*','*y[W])),pos=4,cex=1.5,offset=1,col="grey")
 points(xt,yt,cex=3,pch=16,col="orange")
 text(xt,yt,expression((x[T]*','*y[T])),pos=4,cex=1.5,col="orange")
 text(xs+2*(xw-xs),ys+2*(yw-ys),"W",pos=3,cex=1.5,offset=1,col="grey")
-text(2.3,2,"X",cex=1.5,col="red")
-text(2.1,3.25,"Y",cex=1.5,col="blue")
+text(2.3,2,expression(bar("X")),cex=1.5,col="red")
+text(2.1,3.25,expression(bar("Y")),cex=1.5,col="blue")
 
+#export at 1359x761
