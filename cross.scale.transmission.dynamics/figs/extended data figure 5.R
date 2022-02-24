bcb43d90fd9ba847888e9c699b8a84fa853a7c79
ifmiller <ifmiller@princeton.edu>
2022-02-24 13:34:39 -0500

    rename figure files

--- cross.scale.transmission.dynamics/figs/extended data figure 5.R
+++ cross.scale.transmission.dynamics/figs/extended data figure 5.R
@@ -1,39 +1,43 @@
-library(mgcv)
+# load spore deposition functions and data
+source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/spore deposition functions tilt.R")
+source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/prep.enviro.data.R")
+spore.deposition<-read.csv("~/Documents/GitHub/flax.rust/data/spore counts.csv")
+spore.deposition[which(spore.deposition$Distance.cm==0),"Distance.cm"]<-5 #set distance for "0" spore traps to 5cm
+demog<-read.csv("~/Documents/GitHub/flax.rust/data/Demography.csv")
+demog<-demog[which(demog$year==2020),] #subset to 2020
 
-# load data
-source("~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/plant growth data prep.R")
+# load fitted tilted gaussian plume model parameters
+opt<-list(par=c(5.739690e-07,4.451030e-02, 7.777373e-02))
 
-delta.plant.heights<-subset(delta.plant.heights,time<=8)
+## visualize fit
+pred.mat<-param.search.optim.tilted.plume(opt$par,return.out=T)
 
-# visualize data
+SS.tot<-sum((pred.mat$obs-mean(pred.mat$obs))^2)
+SS.resid<-sum(pred.mat$obs-pred.mat$pred)^2
+R.squared<-1-SS.resid/SS.tot
 
-layout(matrix(c(1,2,3,3),2,2,byrow = T))
-par(mar=c(5,6,2,5))
+par(mfrow=c(2,2),mar=c(5,5,5,5))
+plot(pred.mat$obs,pred.mat$pred,xlab="observed spore deposition",ylab="predicted spore deposition",cex=2,cex.lab=2)
+grid()
+text(65,27,expression(R^2*' = 0.409'),pos=4,offset=0,cex=2)
+segments(65,23,77,23,lwd=1,lty=2)
+text(78,23,"y = x",pos=4,offset=0,cex=2)
+abline(0,1,lty=2,lwd=1)
+mtext("A",adj=1,cex=1.5,line=1)
 
-## histograms
-hist(delta.plant.heights$height,main="",breaks=100,xlab="plant height (cm)",cex.lab=2,cex.axis=2,cex.main=2,panel.first=grid())
-box()
-mtext("A",side=3,adj=.95,line=-3,cex=2)
-hist((delta.plant.heights$height.next-delta.plant.heights$height)/delta.plant.heights$time,main="",breaks=100,xlab="change in plant height per day (cm)",cex.lab=2,cex.axis=2,cex.main=2,panel.first=grid())
-mtext("B",side=3,adj=.95,line=-3,cex=2)
-box()
+plot(log10(pred.mat$obs),log10(pred.mat$pred),xlab=expression(log[10]*" observed spore deposition"),ylab=expression(log[10]*" predicted spore deposition"),cex=2,cex.lab=2)
+grid()
+abline(0,1,lty=2,lwd=1)
+mtext("B",adj=1,cex=1.5,line=1)
 
-## plot trajectories
-par(mar=c(3,6,0,5))
-plot(c(min(plant.heights$date),max(plant.heights$date)),c(0,max(plant.heights$max.height)),type="n",xlab="",ylab="plant height (cm)",cex.lab=2,cex.axis=2,panel.first=grid())
-mtext("C",side=3,adj=.975,line=-3,cex=2)
-i<-0
+plot(pred.mat$obs,pred.mat$pred-pred.mat$obs,xlab="observed spore deposition",ylab="residual",cex=2,cex.lab=2)
+grid()
+abline(h=0,lty=2,lwd=1)
+mtext("C",adj=1,cex=1.5,line=1)
 
-set.seed(8427602)
-plot.cols<-sample(rainbow(180))
+plot(pred.mat$dist,pred.mat$pred-pred.mat$obs,xlab="distance",ylab="residual",cex=2,cex.lab=2)
+grid()
+abline(h=0,lty=2,lwd=1)
+mtext("D",adj=1,cex=1.5,line=1)
 
-for (i in 1:length(unique(plant.heights$tag)))
-{
-  tag<-unique(plant.heights$tag)[i]
-  sub.heights<-plant.heights[which(plant.heights$tag==tag),]
-  sub.heights<-sub.heights[order(sub.heights$date),]
-  points(sub.heights$date,sub.heights$max.height,col=plot.cols[i],type="l",lwd=.5)
-  
-}
-
-#export at 1156 x 738
+#export at 1250 x 878
