b4b81b378366efc2a73835cc447b06d6473a3a38
ifmiller <ifmiller@princeton.edu>
2022-03-17 15:33:26 -0400

    fig updates

--- cross.scale.transmission.dynamics/figs/extended data figure 1 image.R
+++ cross.scale.transmission.dynamics/figs/extended data figure 1 image.R
@@ -51,12 +51,12 @@ rgb_array = aperm(rgb_array, c(2,1,3))
 
 #plot_map(rgb_array)
 
-rgb_contrast = scales::rescale(rgb_array,to=c(.1,1))
+rgb_contrast = scales::rescale(rgb_array,to=c(.2,1))
 
 #plot_map(rgb_contrast)
 
-plot_3d(rgb_contrast, el_matrix, windowsize = c(1000,510), zscale = 15,
-        zoom=.50, phi=32,theta=220,fov=1, background = 'white')
+plot_3d(rgb_contrast, el_matrix, windowsize = c(1250,800), zscale = 15,
+        zoom=.55, phi=32,theta=230,fov=1, background = 'white')
 render_scalebar(limits=c(0, 5, 10),label_unit = "km",position = "W", y=50,offset=50,text_x_offset = 50,
                 scale_length = 1-10/(dim(el_matrix)[2]*30/1000)) # set scale based on 30m^2 pixel size
 
@@ -66,9 +66,9 @@ render_compass(position="N",scale_distance = 1)
 site.cols<-viridis_pal(alpha=1)(20)[c(20,15,6,1)]
 points<-sp::SpatialPoints(data.frame(y=c(-106.868884,-106.996155,-107.019382,-107.021842),x=c(38.821476,38.971273,38.979707,38.967786)),proj4string=sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")) %>% sp::spTransform(raster::crs(elevation_utm))
 render_label(heightmap=el_matrix,text="CC",lat=points@coords[1,2],long=points@coords[1,1],extent=e,textcolor="white",linecolor = site.cols[1],z=200,zscale=15,textsize = 2,linewidth = 4)
-render_label(heightmap=el_matrix,text="BT",lat=points@coords[2,2],long=points@coords[2,1],extent=e,textcolor="white",linecolor = site.cols[2],z=750,zscale=15,textsize = 2.5,linewidth = 4)
-render_label(heightmap=el_matrix,text="GM",lat=points@coords[3,2],long=points@coords[3,1],extent=e,textcolor="white",linecolor = site.cols[3],z=2000,zscale=15,textsize = 2.5,linewidth = 4)
-render_label(heightmap=el_matrix,text="HM",lat=points@coords[4,2],long=points@coords[4,1],extent=e,textcolor="white",linecolor = site.cols[4],z=2500,zscale=15,textsize = 2.5,linewidth = 4)
+render_label(heightmap=el_matrix,text="BT",lat=points@coords[2,2],long=points@coords[2,1],extent=e,textcolor="white",linecolor = site.cols[2],z=750,zscale=15,textsize = 2,linewidth = 4)
+render_label(heightmap=el_matrix,text="GM",lat=points@coords[3,2],long=points@coords[3,1],extent=e,textcolor="white",linecolor = site.cols[3],z=2000,zscale=15,textsize = 2,linewidth = 4)
+render_label(heightmap=el_matrix,text="HM",lat=points@coords[4,2],long=points@coords[4,1],extent=e,textcolor="white",linecolor = site.cols[4],z=2500,zscale=15,textsize = 2,linewidth = 4)
 
 render_snapshot() #save as ~/Documents/GitHub/flax.rust/cross.scale.transmission.dynamics/figs/figure.1.files/B.jpg
-# export at dimensions 940x696, then crop to minimum bounding dimensions
+# export at dimensions 1275x788, then crop to minimum bounding dimensions
